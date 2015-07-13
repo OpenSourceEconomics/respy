6fb1da68ebf67424f7c6d5e76a70a1b7cb8459b9
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-07-13 09:52:27 +0200

    exploring test D

--- development/driver.py
+++ development/driver.py
@@ -13,33 +13,14 @@ sys.path.insert(0, os.environ['ROBUPY'])
 # project library
 from robupy import *
 
-import numpy as np
-import robupy.fort.fortran_functions as fort
+# Relative Criterion
+robupy_obj = read('tests/fixed/third.robupy.ini')
 
-num_cols = 4
-A = np.identity(num_cols)
+robupy_obj = solve(robupy_obj)
 
+simulate(robupy_obj)
 
+cleanup()
 
-
-np.random.seed(2)
-for _ in range(100000):
-
-    x = np.random.random(2)
-
-    matrix = (np.random.multivariate_normal(np.zeros(4),
-                                np.identity(4), (4)))
-
-    cov = np.dot(matrix, matrix.T)
-
-
-    level = np.random.random(1)[0]
-    print('\n\n NEW TASK')
-    print(cov)
-    py = np.linalg.inv(cov)
-    f90 = fort.inverse(cov, 4)
-
-    print(py - f90)
-
-    np.testing.assert_allclose(py, f90, rtol = 1e-06)
-    #assert (np.abs(py - f90) < 10e-6)
+# Assert unchanged value
+assert abs(robupy_obj.get_attr('emax')[0, :1] - 55931.87437459) < 0.00000001
