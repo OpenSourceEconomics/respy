e7aff1130fa401fa2df5efdb8a88f38e6197e5b6
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-08-24 15:37:03 +0200

    restarting efforts

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
   packages = ['robupy', 'robupy.checks', 'robupy.fort', 'robupy.tests'], # this
   # must be the same as the name above
     package_data = {'robupy': ['fort/*.f90'],},
-  version = '0.1.3',
-  description = 'Toolbox to explore robust dynamic discrete choice model',
+  version = '0.1.4',
+  description = 'Toolbox to explore robust dynamic discrete choice models',
   author = 'Philipp Eisenhauer',
   author_email = 'eisenhauer@policy-lab.org',
-  url = 'https://github.com/grmToolbox/package', # use the URL to the github repo
+  url = 'https://github.com/robustToolbox/package', # use the URL to the github repo
   keywords = ['Economics', 'Dynamic Discrete Choice Model', 'Robustness'], # arbitrary keywords
   classifiers = [],
   install_requires=['numpy', 'pandas', 'coveralls', 'nose', 'scipy'],
