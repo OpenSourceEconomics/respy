c66ee01de7771ec27c70ad509e005819fdb47824
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-08-27 17:02:49 +0200

    improved MC creation

--- development/monte_carlo/original_codes/f95/create.py
+++ development/monte_carlo/original_codes/f95/create.py
@@ -3,41 +3,46 @@
 papers.
 """
 
-
+# standard library
 import os
 import shutil
 
-
+# Compile executable
 os.system(' gfortran -o dp3asim dp3asim.f95')
 
-# Prepare directories
+# Ensure fresh start
 for which in ['one', 'two', 'three']:
-    shutil.rmtree('data_' + which)
 
-for i, which in enumerate(['one', 'two', 'three']):
-
+    # Clean directories
     try:
-        os.mkdir('data_' + which)
+        shutil.rmtree('data_' + which)
     except OSError:
         pass
 
-    try:
-        os.symlink('in' + str(i + 1) + '.txt', 'in.txt')
-    except OSError:
-        pass
-
-    os.system('./dp3asim')
-
+    # Clean model specification file
     try:
         os.remove('in.txt')
     except OSError:
         pass
 
+# Iterate over model specifications
+for i, which in enumerate(['one', 'two', 'three']):
+
+    # Storage for results
+    os.mkdir('data_' + which)
+
+    # Link to model specification
+    os.symlink('in' + str(i + 1) + '.txt', 'in.txt')
+
+    # Solve and simulate model
+    os.system('./dp3asim')
+
+    # Clean
+    os.remove('in.txt')
+
     # Move material for inspection
     for file_ in ['ftest.txt', 'otest.txt']:
-
         shutil.move(file_, 'data_' + which + '/')
-
 
 # Final cleanup
 for file_ in ['dp3asim', 'imsl_replacements.mod']:
