ef73f2c071e4e564caf1553ca144b4fbbde4214d
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-08-28 14:30:34 +0200

    edits

--- robupy/simulate.py
+++ robupy/simulate.py
@@ -118,7 +118,10 @@ def simulate(robupy_obj):
             if period == (num_periods - 1):
                 future_payoffs = np.zeros(4)
             else:
-                future_payoffs = perf_lib.get_future_payoffs(edu_max, edu_start, mapping_state_idx, period, emax, k, states_all)
+                future_payoffs = perf_lib.get_future_payoffs(edu_max, edu_start,
+                                                             mapping_state_idx,
+                                                             period, emax, k,
+                                                             states_all)
 
             # Calculate total utilities
             total_payoffs = period_payoffs_ex_post + delta * future_payoffs
