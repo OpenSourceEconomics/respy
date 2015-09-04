bb53c2d30d51118df2e094acbf1bac7eccd4ff1f
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-09-04 07:47:06 +0200

    before benchmarking exercise

--- development/tests/random/modules/battery.py
+++ development/tests/random/modules/battery.py
@@ -11,7 +11,7 @@ import sys
 import os
 
 # project library
-from modules.auxiliary import compile_package
+from modules.auxiliary import compile_package, transform_robupy_to_restud
 
 # ROBUPY import
 sys.path.insert(0, os.environ['ROBUPY'])
@@ -84,66 +84,14 @@ def test_98():
     # Perform toolbox actions
     robupy_obj = read('test.robupy.ini')
 
-    init_dict = robupy_obj.get_attr('init_dict')
-
     # This flag aligns the random components between the RESTUD program and
     # ROBUPY package. The existence of the file leads to the RESTUD program
     # to write out the random components.
-    robupy_obj.is_restud = True
-    open('.write_out', 'a').close()
+    robupy_obj.is_restud = True; open('.write_out', 'a').close()
 
-    with open('in.txt', 'w') as file_:
+    init_dict = robupy_obj.get_attr('init_dict')
 
-        # Write out some basic information about the problem.
-        num_agents = init_dict['SIMULATION']['agents']
-        num_periods = init_dict['BASICS']['periods']
-        num_draws = init_dict['SOLUTION']['draws']
-        file_.write(' {0:03d} {1:05d} {2:06d} {3:06f}'
-            ' {4:06f}\n'.format(num_periods, num_agents, num_draws,-99.0,
-            500.0))
-
-        # Write out coefficients for the two occupations.
-        for label in ['A', 'B']:
-            coeffs = [init_dict[label]['int']] + init_dict[label]['coeff']
-            line = ' {0:10.6f} {1:10.6f} {2:10.6f} {3:10.6f}  {4:10.6f}' \
-                    ' {5:10.6f}\n'.format(*coeffs)
-            file_.write(line)
-
-        # Write out coefficients for education and home payoffs as well as
-        # the discount factor. The intercept is scaled. This is later undone
-        # again in the original FORTRAN code.
-        edu_int = init_dict['EDUCATION']['int'] / 1000; edu_coeffs = [edu_int]
-        home = init_dict['HOME']['int'] / 1000
-        for j in range(2):
-            edu_coeffs += [-init_dict['EDUCATION']['coeff'][j] / 1000]
-        delta = init_dict['BASICS']['delta']
-        coeffs = edu_coeffs + [home, delta]
-        line = ' {0:10.6f} {1:10.6f} {2:10.6f} {3:10.6f}' \
-                ' {4:10.6f}\n'.format(*coeffs)
-        file_.write(line)
-
-        # Write out coefficients of correlation, which need to be constructed
-        # based on the covariance matrix.
-        shocks = init_dict['SHOCKS']; rho = np.identity(4)
-        rho_10 = shocks[1][0] / (np.sqrt(shocks[1][1]) * np.sqrt(shocks[0][0]))
-        rho_20 = shocks[2][0] / (np.sqrt(shocks[2][2]) * np.sqrt(shocks[0][0]))
-        rho_30 = shocks[3][0] / (np.sqrt(shocks[3][3]) * np.sqrt(shocks[0][0]))
-        rho_21 = shocks[2][1] / (np.sqrt(shocks[2][2]) * np.sqrt(shocks[1][1]))
-        rho_31 = shocks[3][1] / (np.sqrt(shocks[3][3]) * np.sqrt(shocks[1][1]))
-        rho_32 = shocks[3][2] / (np.sqrt(shocks[3][3]) * np.sqrt(shocks[2][2]))
-        rho[1, 0] = rho_10; rho[2, 0] = rho_20; rho[3, 0] = rho_30
-        rho[2, 1] = rho_21; rho[3, 1] = rho_31; rho[3, 2] = rho_32
-        for j in range(4):
-            line = ' {0:10.5f} {1:10.5f} {2:10.5f} ' \
-                   ' {3:10.5f}\n'.format(*rho[j, :])
-            file_.write(line)
-
-        # Write out standard deviations. Scaling for standard deviations in the
-        # education and home equation required. This is undone again in the
-        # original FORTRAN code.
-        sigmas = np.sqrt(np.diag(shocks)); sigmas[2:] = sigmas[2:]/1000
-        line = '{0:10.5f} {1:10.5f} {2:10.5f} {3:10.5f}\n'.format(*sigmas)
-        file_.write(line)
+    transform_robupy_to_restud(init_dict)
 
     # Solve model using RESTUD code.
     os.system('./modules/dp3asim > /dev/null 2>&1')
