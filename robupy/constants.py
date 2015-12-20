f0b0d17a95b47053a33ebe754ecce03870546447
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-12-20 14:54:00 +0100

    constants not checked in

---
+++ robupy/constants.py
@@ -0,0 +1,7 @@
+""" Module for program constants used across the ROBUPY package. This is
+aligned with the constants from the FORTRAN implementation.
+"""
+
+MISSING_INT = -99
+MISSING_DBLE = -99.00
+HUGE_DBLE = 1.0e10
