e87f5ae331e1e230ea7a2816f39650477af230ef
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2015-12-28 14:04:52 +0100

    fixed optimization problems

--- development/analyses/drafts/model_misspecification/modules/auxiliary.py
+++ development/analyses/drafts/model_misspecification/modules/auxiliary.py
@@ -12,10 +12,8 @@ import glob
 import os
 
 # project library
-from robupy.tests.random_init import print_random_dict
-
+from robupy.clsRobupy import RobupyCls
 from robupy import solve
-from robupy import read
 
 
 def distribute_arguments(parser):
@@ -26,97 +24,77 @@ def distribute_arguments(parser):
 
     # Extract arguments
     is_restart = args.is_restart
+    num_procs = args.num_procs
     is_debug = args.is_debug
-    level = args.level
+    levels = args.levels
 
     # Check arguments
-    assert (isinstance(level, float))
-    assert (level >= 0.00)
+    assert (isinstance(levels, list))
+    assert (np.all(levels) >= 0.00)
     assert (is_restart in [True, False])
     assert (is_debug in [True, False])
+    assert (isinstance(num_procs, int))
+    assert (num_procs > 0)
 
     # Check for restart material
     if is_restart:
-        assert (os.path.exists('%03.3f/true/base_choices.pkl' % level))
+        for level in levels:
+            assert (os.path.exists('%03.3f/true/base_choices.pkl' % level))
 
     # Finishing
-    return level, is_restart, is_debug
+    return levels, num_procs, is_restart, is_debug
 
 
 def prepare_directory(is_restart, name):
-    # Deal with restarted estimations as well
+    """ Prepare directory structure depending whether a fresh start or we are
+    building on existing results.
+    """
     if not is_restart:
         if os.path.exists(name):
             shutil.rmtree(name)
-
         os.mkdir(name)
     else:
-
         os.chdir(name)
-
         for obj in glob.glob('*'):
             # Retain results from previous run
-            if 'true' in obj:
-                continue
+            if 'true' in obj: continue
             # Delete files and directories
             try:
                 shutil.rmtree(obj)
             except OSError:
                 os.unlink(obj)
-
         os.chdir('../')
 
 
-def solve_true_economy(level, is_debug):
-
+def solve_true_economy(level, init_dict, is_debug):
+    """ Solve and store true economy.
+    """
+    # Prepare directory structure
     os.mkdir('true'), os.chdir('true')
-    robupy_obj = read('../model.robupy.ini')
-    init_dict = robupy_obj.get_attr('init_dict')
+    # Modify initialization dictionary
     init_dict['AMBIGUITY']['level'] = level
-
     if is_debug:
         init_dict['BASICS']['periods'] = 5
-
-    print_random_dict(init_dict)
-    shutil.move('test.robupy.ini', 'model.robupy.ini')
-    base_choices = get_baseline('model.robupy.ini')
-
+    # Solve economy choices
+    solve(_get_robupy_obj(init_dict))
+    # Get baseline choice distributions
+    base_choices = get_period_choices()
     # Store material required for restarting an estimation run
     pkl.dump(base_choices, open('base_choices.pkl', 'wb'))
-
     os.chdir('../')
 
 
-
-def get_baseline(name):
+def criterion_function(point, base_choices, init_dict, is_debug):
     """ Get the baseline distribution.
     """
-    # Solve baseline distribution
-    robupy_obj = read(name)
-    solve(robupy_obj)
-    # Get baseline choice distributions
-    base_choices = get_period_choices()
-    # Finishing
-    return base_choices
-
-
-def criterion_function(point, base_choices, is_debug):
-    """ Get the baseline distribution.
-    """
-    # Write out the modified baseline initialization.
-    robupy_obj = read('test.robupy.ini')
-    init_dict = robupy_obj.get_attr('init_dict')
+    # Auxiliary objects
+    scaling = 100000.00
     # Set relevant values
-    init_dict['EDUCATION']['int'] = float(point)
-
+    init_dict['EDUCATION']['int'] = float(point)*scaling
     if is_debug:
         init_dict['BASICS']['periods'] = 5
-
-    # Write to file
-    print_random_dict(init_dict)
     # Solve requested model
-    robupy_obj = read('test.robupy.ini')
-    solve(robupy_obj)
+    solve(_get_robupy_obj(init_dict))
     # Get choice probabilities
     alternative_choices = get_period_choices()
     # Calculate squared mean-deviation of all transition probabilities
@@ -152,26 +130,28 @@ def get_period_choices():
     return choices
 
 
-def solve_estimated_economy(opt):
+def solve_estimated_economy(opt, init_dict, is_debug):
     """ Collect information about estimated economy by updating an initialization
     file with the resulting estimate for the intercept and solving the
     resulting economy.
     """
-    # Read baseline initialization file
-    init_dict = read('test.robupy.ini').get_attr('init_dict')
-
     # Switch into extra directory to store the results
     os.mkdir('estimated'), os.chdir('estimated')
-
     # Update initialization file with result from estimation and write to disk
     init_dict['EDUCATION']['int'] = float(opt['x'])
-    # TODO: Remove later
-    init_dict['BASICS']['periods'] = 5
-
-    print_random_dict(init_dict)
-
-    # Start solution and estimation of resulting economy
-    shutil.move('test.robupy.ini', 'model.robupy.ini')
-    solve(read('model.robupy.ini'))
-
+    if is_debug:
+        init_dict['BASICS']['periods'] = 5
+    # Solve the basic economy
+    solve(_get_robupy_obj(init_dict))
+    # Return to root dictionary
     os.chdir('../')
+
+
+def _get_robupy_obj(init_dict):
+    """ Get the object to pass in the solution method.
+    """
+    robupy_obj = RobupyCls()
+    robupy_obj.set_attr('init_dict', init_dict)
+    robupy_obj.lock()
+    # Finishing
+    return robupy_obj
--- development/analyses/drafts/model_misspecification/modules/create.py
+++ development/analyses/drafts/model_misspecification/modules/create.py
@@ -6,6 +6,8 @@ level of ambiguity and then fit a risk-only model.
 
 # standard library
 from scipy.optimize import minimize
+from multiprocessing import Pool
+from functools import partial
 
 import pickle as pkl
 
@@ -31,7 +33,6 @@ from auxiliary import solve_true_economy
 from auxiliary import criterion_function
 from auxiliary import prepare_directory
 
-from robupy.tests.random_init import print_random_dict
 from modules.auxiliary import compile_package
 
 from robupy import read
@@ -41,20 +42,12 @@ from robupy import read
 '''
 
 
-def run(level, is_restart, is_debug):
+def run(is_restart, is_debug, level):
     """ Inspect the implications of model misspecification for the estimates
     of psychic costs.
     """
     # Auxiliary objects
     name = '%03.3f' % level
-
-    # Ensure that fast version of package is available. This is a little more
-    # complicated than usual as the compiler on acropolis does use other
-    # debugging flags and thus no debugging is requested.
-    if 'acropolis' in socket.gethostname():
-        compile_package('--fortran', True)
-    else:
-        compile_package('--fortran --debug', True)
 
     # Prepare the directory structure. We initialize a fresh directory named
     # by the level of ambiguity in the baseline economy. If restart, then the
@@ -62,33 +55,30 @@ def run(level, is_restart, is_debug):
     # economy is retained.
     prepare_directory(is_restart, name)
 
-    # Now we move into the prepared subdirectory.
+    # Now we move into the prepared subdirectory and obtain the parametrization
+    # of the baseline economies.
     os.chdir(name)
     shutil.copy(SPEC_DIR + '/data_one.robupy.ini', 'model.robupy.ini')
+
+    # Process baseline initialization dictionary.
+    init_dict = read('model.robupy.ini').get_attr('init_dict')
 
     # Store the information about the true underlying data generating process in
     # a subdirectory and read in base choices. Or just read it in from a
     # previous run.
     if not is_restart:
-        solve_true_economy(level, is_debug)
-
+        solve_true_economy(level, init_dict, is_debug)
     base_choices = pkl.load(open('true/base_choices.pkl', 'rb'))
 
-    # Prepare initialization file
-    robupy_obj = read('model.robupy.ini')
-    init_dict = robupy_obj.get_attr('init_dict')
-
-    # Modification from baseline initialization file
+    # Modification from baseline initialization file. This is not really
+    # necessary but improves readability.
     init_dict['AMBIGUITY']['level'] = 0.00
-
-    # Finalize initialization file and solve model
-    print_random_dict(init_dict)
 
     # Criterion function uses update. We optimize over the intercept in the
     # reward function.
     x0 = init_dict['EDUCATION']['int']
-    opt = minimize(criterion_function, x0, args=(base_choices, is_debug),
-                   method="Nelder-Mead")
+    opt = minimize(criterion_function, x0, args=(base_choices, init_dict,
+                    is_debug), method="Nelder-Mead")
 
     # Write out some basic information to a file.
     with open('misspecification.robupy.log', 'a') as file_:
@@ -103,7 +93,7 @@ def run(level, is_restart, is_debug):
         file_.write('    Message ' + opt['message'] + '\n\n')
 
     # Solve the estimated economy to compare
-    solve_estimated_economy(opt)
+    solve_estimated_economy(opt, init_dict, is_debug)
 
     # Cleanup files generated during estimation
     for file_ in glob.glob('*.robupy.*'):
@@ -111,7 +101,7 @@ def run(level, is_restart, is_debug):
         if 'misspecification' in file_:
             continue
         os.unlink(file_)
-
+    os.chdir('../')
 
 ''' Execution of module as script.
 '''
@@ -122,8 +112,8 @@ if __name__ == '__main__':
         description='Assess implications of model misspecification.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--level', action='store', type=float, dest='level',
-        default=0.00, help='level of ambiguity in true economy')
+    parser.add_argument('--levels', action='store', type=float, dest='levels',
+        required=True, nargs='+', help='level of ambiguity in true economy')
 
     parser.add_argument('--restart', action='store_true', dest='is_restart',
         help='restart using existing results')
@@ -131,6 +121,20 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true', dest='is_debug',
         help='only five periods')
 
-    level, is_restart, is_debug = distribute_arguments(parser)
+    parser.add_argument('--procs', action='store', type=int, dest='num_procs',
+         default=1, help='use multiple processors')
 
-    run(level, is_restart, is_debug)
+    # Distribute attributes
+    levels, num_procs, is_restart, is_debug = distribute_arguments(parser)
+
+    # Ensure that fast version of package is available. This is a little more
+    # complicated than usual as the compiler on acropolis does use other
+    # debugging flags and thus no debugging is requested.
+    if 'acropolis' in socket.gethostname():
+        compile_package('--fortran', True)
+    else:
+        compile_package('--fortran --debug', True)
+
+    # Set up pool for processors for parallel execution.
+    process_tasks = partial(run, is_restart, is_debug)
+    Pool(num_procs).map(process_tasks, levels)
