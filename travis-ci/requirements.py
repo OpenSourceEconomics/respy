31eaa77948fa4852bfc52a9dcac9a52059697c19
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-02-29 16:09:55 +0100

    alternative to get f2py3 on traivs

--- travis-ci/requirements.py
+++ travis-ci/requirements.py
@@ -13,7 +13,7 @@ os.system('sudo apt-get update -qq')
 packages = ['build-essential', 'gfortran', 'python3-pip', 'python-pip', 'git',
             'libblas-dev', 'libatlas-base-dev', 'liblapack-dev',
             'libyaml-cpp-dev', 'cython3', 'python-dev', 'python3-dev',
-            'libevent-dev']
+            'libevent-dev', 'python3-numpy']
 
 for package in packages:
     os.system('sudo apt-get install -y ' + package)
