7ce4aca9e635f997ed01bab16c3c1958aed50e13
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-02-29 16:38:02 +0100

    edits

--- travis-ci/requirements.py
+++ travis-ci/requirements.py
@@ -13,7 +13,7 @@ os.system('sudo apt-get update -qq')
 packages = ['build-essential', 'gfortran', 'python3-pip', 'python-pip', 'git',
             'libblas-dev', 'libatlas-base-dev', 'liblapack-dev',
             'libyaml-cpp-dev', 'cython3', 'python-dev', 'python3-dev',
-            'libevent-dev', 'python3-numpy']
+            'libevent-dev']
 
 for package in packages:
     os.system('sudo apt-get install -y ' + package)
--- travis-ci/run.py
+++ travis-ci/run.py
@@ -13,16 +13,24 @@ ROBUPY_DIR = os.path.abspath(os.path.dirname(__file__))
 ROBUPY_DIR = ROBUPY_DIR.replace('/travis-ci', '/robupy')
 
 # Compile the package as possible.
+os.system('echo $HOSTNAME')
+
+os.system('ln -sf /home/travis/virtualenv/python3.4.2/bin/f2py /home/travis/virtualenv/python3.4.2/bin/f2py3')
+
+
 os.chdir(ROBUPY_DIR)
 os.system('./waf configure build --fortran --debug')
 os.chdir(CURRENT_DIR)
-
+    
 os.system('f2py')
 os.system('f2py3')
 
 
 os.system('which f2py')
 os.system('which f2py3')
+
+
+
 
 # Tests
 os.system('nosetests --with-coverage --cover-package=robupy --exe')
