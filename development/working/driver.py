5051782593a958ee1e152df16b718a698d39adc6
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-03-06 10:51:50 +0100

    now moving to FORTRAN and F2PY work

--- development/working/driver.py
+++ development/working/driver.py
@@ -6,6 +6,7 @@ proposed in Keane & Wolpin (1994).
 # standard library
 import numpy as np
 
+import scipy
 import sys
 import os
 
@@ -13,43 +14,42 @@ import os
 sys.path.insert(0, os.environ['ROBUPY'] + '/development/tests/random')
 sys.path.insert(0, os.environ['ROBUPY'])
 
-# project library
+# RobuPy library
 from robupy import *
 
 from robupy.auxiliary import opt_get_model_parameters
 from robupy.auxiliary import opt_get_optim_parameters
 from robupy.auxiliary import distribute_model_paras
 
+# testing battery
+from modules.auxiliary import compile_package
+
 # Read in baseline initialization file
-#compile_package('--fortran --debug', False)
+compile_package('--fortran --debug', False)
 
 np.random.seed(123)
 
-# Generate random initialization file
-robupy_obj = read('original.robupy.ini')
+import robupy.python.f2py.f2py_debug as fort
+
+# Draw random requests for testing purposes.
+num_draws = np.random.random_integers(2, 1000)
+dim = np.random.random_integers(1, 6)
+mean = np.random.uniform(-0.5, 0.5, (dim))
+
+matrix = (np.random.multivariate_normal(np.zeros(dim), np.identity(dim), dim))
+cov = np.dot(matrix, matrix.T)
+
+# Singular Value Decomposition
+py = scipy.linalg.svd(matrix)[0]
+f90 = fort.wrapper_svd(matrix, dim)[0]
+
+for i in range(3):
+    np.testing.assert_allclose(py[i], f90[i], rtol=1e-05, atol=1e-06)
+
+''' F
+'''
+robupy_obj = read('test.robupy.ini')
 
 solve(robupy_obj)
 
-simulate(robupy_obj)
 
-data_frame = process('data.robupy.dat', robupy_obj)
-
-is_debug = robupy_obj.get_attr('is_debug')
-
-model_paras = robupy_obj.get_attr('model_paras')
-
-eps_cholesky = model_paras['eps_cholesky']
-
-model_paras = robupy_obj.get_attr("model_paras")
-
-args = distribute_model_paras(model_paras, is_debug)
-
-x = opt_get_optim_parameters(*args, is_debug=is_debug)
-
-
-# Construct auxiliary objects
-args = opt_get_model_parameters(x, is_debug)
-
-base = evaluate(robupy_obj, data_frame)
-
-np.testing.assert_almost_equal(base, 0.18991613969942756)
