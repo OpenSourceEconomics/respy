bad99135bbe40180a49e31829979804fb68793e7
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-03-29 21:09:36 +0200

    travis fix

--- travis-ci/run.py
+++ travis-ci/run.py
@@ -6,17 +6,17 @@
 import pytest
 import os
 
+# If the script is run on TRAVIS-CI, then I need to create a link to F2PY3. So
+# far I was unable to figure out why that is the case.
+if 'TRAVIS' in os.environ.keys():
+    os.system('ln -sf /home/travis/virtualenv/python3.4.2/bin/f2py /home/travis/virtualenv/python3.4.2/bin/f2py3')
+
 # Build the package
 os.chdir('robupy')
 
 os.system('./waf configure build')
 
 os.chdir('../')
-
-# If the script is run on TRAVIS-CI, then I need to create a link to F2PY3. So
-# far I was unable to figure out why that is the case.
-if 'TRAVIS' in os.environ.keys():
-    os.system('ln -sf /home/travis/virtualenv/python3.4.2/bin/f2py /home/travis/virtualenv/python3.4.2/bin/f2py3')
 
 # Run PYTEST battery, some tests are expected to fail due to small numerical
 # differences between PYTHON and FORTRAN implementations.
