82b25c27d568dc543e5d7c9676e2836c086dc3b6
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-03-30 21:28:33 +0200

    refactoring, all running

--- robupy/fortran/build_robufort.py
+++ robupy/fortran/build_robufort.py
@@ -42,9 +42,9 @@ def robufort_build(self, is_debug=False):
           'shared/shared_constants.f90 shared/shared_auxiliary.f90 ' \
           'robufort_slsqp.f  solve/solve_emax.f90 ' \
           'solve/solve_risk.f90 solve/solve_ambiguity.f90 solve/solve_auxiliary.f90 ' \
-          'solve/solve.f90  evaluate/evaluate_auxiliary.f90 ' \
-          'evaluate/evaluate.f90 estimate/estimate_auxiliary.f90 ' \
-          'simulate/simulate.f90 ' \
+          'solve/solve_fortran.f90  evaluate/evaluate_auxiliary.f90 ' \
+          'evaluate/evaluate_fortran.f90 estimate/estimate_auxiliary.f90 ' \
+          'simulate/simulate_fortran.f90 ' \
           'robufort.f90 -L/usr/lib/lapack -llapack'
 
     os.system(cmd)
