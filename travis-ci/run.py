11f9b4d64a82f83f917deb5fad59317975c3d2cb
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-03-30 12:55:49 +0200

    te

--- travis-ci/run.py
+++ travis-ci/run.py
@@ -20,7 +20,7 @@ os.chdir('../')
 
 # Run PYTEST battery, some tests are expected to fail due to small numerical
 # differences between PYTHON and FORTRAN implementations.
-os.system('pip uninstall coverage')
+os.system('pip uninstall -y coverage')
 os.system('pip install coverage==3.7')
 os.system('pip install pytest-cov==2.2.1')
 pytest.main('--cov=robupy -v -s')
