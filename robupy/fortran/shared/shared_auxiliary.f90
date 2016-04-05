4aa1d9b086e8c5210db4990bdd249ad36fbc0796
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-04-05 18:20:45 +0200

    all working, cleanup

--- robupy/fortran/shared/shared_auxiliary.f90
+++ robupy/fortran/shared/shared_auxiliary.f90
@@ -10,11 +10,6 @@ MODULE shared_auxiliary
 
     IMPLICIT NONE
 
-    ! External procedures defined in LAPACK
-    EXTERNAL DGETRF
-    EXTERNAL DGETRI
-    EXTERNAL DPOTRF
-
     PUBLIC
 
     ! Polymorphisms
@@ -23,6 +18,78 @@ MODULE shared_auxiliary
         MODULE PROCEDURE clip_value_1, clip_value_2
 
     END INTERFACE
+
+
+    INTERFACE
+
+        SUBROUTINE DPOTRF(UPLO, N, A, LDA, INFO)
+
+            !/* external modules    */
+
+            USE shared_constants
+
+            !/* external objects    */
+
+            REAL(our_dble), INTENT(INOUT)   :: A(N, N)
+ 
+            INTEGER(our_int), INTENT(IN)    :: INFO
+            INTEGER(our_int), INTENT(IN)    :: LDA
+            INTEGER(our_int), INTENT(IN)    :: N
+
+            CHARACTER(1), INTENT(IN)        :: UPLO
+
+        END SUBROUTINE
+
+    END INTERFACE
+
+
+    INTERFACE
+
+        SUBROUTINE DGETRF(M, N, A, LDA, IPIV, INFO)
+
+            !/* external modules    */
+
+            USE shared_constants
+
+            !/* external objects    */
+
+            INTEGER(our_int), INTENT(OUT)   :: IPIV(N)
+            INTEGER(our_int), INTENT(OUT)   :: INFO
+
+            REAL(our_dble), INTENT(INOUT)   :: A(N, N)
+
+            INTEGER(our_int), INTENT(IN)    :: LDA
+            INTEGER(our_int), INTENT(IN)    :: M
+            INTEGER(our_int), INTENT(IN)    :: N
+
+         END SUBROUTINE
+
+    END INTERFACE
+
+
+    INTERFACE
+
+        SUBROUTINE DGETRI(N, A, LDA, IPIV, WORK, LWORK, INFO)
+            
+            !/* external modules    */
+
+            USE shared_constants
+
+            !/* external objects    */
+
+            REAL(our_dble), INTENT(INOUT)   :: WORK(LWORK)
+            REAL(our_dble), INTENT(INOUT)   :: A(N, N)
+
+            INTEGER(our_int), INTENT(IN)    :: IPIV(N)
+            INTEGER(our_int), INTENT(IN)    :: LWORK
+            INTEGER(our_int), INTENT(IN)    :: INFO
+            INTEGER(our_int), INTENT(IN)    :: LDA
+            INTEGER(our_int), INTENT(IN)    :: N
+
+         END SUBROUTINE
+
+    END INTERFACE
+
 
  CONTAINS
 !*******************************************************************************
