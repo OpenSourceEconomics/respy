9ce19708f77c950b34fdfa03063a8a11652aff80
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-04-25 10:35:37 +0200

    renaming to respy

--- development/vagrant/bin/initialize_envs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/vagrant/.profile
-mkvirtualenv -p /usr/bin/python3 $WORKON_HOME/robustToolbox
-cd /home/vagrant/robustToolbox/package
-workon robustToolbox
-cat requirements.txt | xargs -n 1 pip install
-
-# Installation of some additional packages used when working with the toolbox.
-pip install matplotlib; pip install paramiko
