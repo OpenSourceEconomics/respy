98d271f34de016b3e9704c3e70e1f2cc598be0a7
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-04-29 19:32:35 +0200

    another attempt

--- travis-ci/run.py
+++ travis-ci/run.py
@@ -3,7 +3,6 @@
 """
 
 # standard library
-import pytest
 import os
 
 # If the script is run on TRAVIS-CI, then I need to create a link to F2PY3. So
@@ -11,22 +10,12 @@ import os
 if 'TRAVIS' in os.environ.keys():
     os.system('ln -sf /home/travis/virtualenv/python3.4.2/bin/f2py /home/travis/virtualenv/python3.4.2/bin/f2py3')
 
-# Build the package.
-assert os.system('python setup.py build') == 0
-
-# TODO: BE REMOVED
-os.system('pip install pytest-cov==2.2.1')
-pytest.main('--cov=respy -v -s -m"(not slow)"')
-
-
-#assert os.system('py.test --cov=respy -v -s -m"(not slow)" -x') == 0
-
-
-print("STARTING TOX")
 # TOX automation
+# TODO: Start with getting 
 os.system('pip install tox')
 assert os.system('tox -v -e py35') == 0
 
 # Run PYTEST battery again for coverage statistic.
+assert os.system('pip install -e .') == 0
 os.system('pip install pytest-cov==2.2.1')
 assert os.system('py.test --cov=respy -v -s -m"(not slow)" -x') == 0
