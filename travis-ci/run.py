5c5a41a611073a5904d6aeabd0b2fc42c4202879
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-04-29 12:44:43 +0200

    minor improvements to travis

--- travis-ci/run.py
+++ travis-ci/run.py
@@ -3,7 +3,6 @@
 """
 
 # standard library
-import pytest
 import os
 
 # If the script is run on TRAVIS-CI, then I need to create a link to F2PY3. So
@@ -12,12 +11,13 @@ if 'TRAVIS' in os.environ.keys():
     os.system('ln -sf /home/travis/virtualenv/python3.4.2/bin/f2py /home/travis/virtualenv/python3.4.2/bin/f2py3')
 
 # Build the package.
-os.system('python setup.py build')
+assert os.system('python setup.py build') == 0
 
-# Run PYTEST battery, some tests are expected to fail due to small numerical
+# Run PYTEST battery, some tests might fail due to small numerical
 # differences between PYTHON and FORTRAN implementations.
 os.system('pip install pytest-cov==2.2.1')
-pytest.main('--cov=respy -v -s -m"(not slow)"')
+assert os.system('py.test --cov=respy -v -s -m"(not slow) -x') == 0
 
 # TOX automation
-os.system('pip install tox; tox')
+os.system('pip install tox')
+assert os.system('tox -v') == 0
