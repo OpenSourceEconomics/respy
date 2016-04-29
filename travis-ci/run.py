ff3d7ad77221277d4d4c14fee27b2b234aceee24
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-04-29 21:48:40 +0200

    edits

--- travis-ci/run.py
+++ travis-ci/run.py
@@ -5,21 +5,11 @@
 # standard library
 import os
 
-# If the script is run on TRAVIS-CI, then I need to create a link to F2PY3. So
-# far I was unable to figure out why that is the case.
-if 'TRAVIS' in os.environ.keys():
-    os.system('ln -sf /home/travis/virtualenv/python3.4.2/bin/f2py /home/travis/virtualenv/python3.4.2/bin/f2py3')
-
 # Run PYTEST battery again for coverage statistic.
 assert os.system('pip install -e .') == 0
 os.system('pip install pytest-cov==2.2.1')
-os.system('py.test --cov=respy -v -s -m"(not slow)" -x') == 0
+assert os.system('py.test --cov=respy -v -s -m"(not slow)" -x') == 0
 
 # TOX automation
 os.system('pip install tox')
 os.system('tox -v') == 0
-os.system('cat /home/travis/build/restudToolbox/package/.tox/py27/log/*.log')
-os.system('ls /home/travis/build/restudToolbox/package/.tox/py27/bin')
-
-os.system('cat /home/travis/build/restudToolbox/package/.tox/py35/log/*.log')
-os.system('ls /home/travis/build/restudToolbox/package/.tox/py35/bin')
