6c2a3287b3657a10258ddfcfb94ba7f01b30c262
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-04-30 13:15:01 +0200

    cleanup continued

--- tools/travis_run.py
+++ tools/travis_run.py
@@ -5,12 +5,12 @@
 # standard library
 import os
 
+# TOX automation
+os.system('pip install tox')
+assert os.system('tox -v') == 0
+
 # Run PYTEST battery again on package in development mode. This ensure that the 
 # current implementation is working properly. 
 assert os.system('pip install -e .') == 0
 os.system('pip install pytest-cov==2.2.1')
 assert os.system('py.test --cov=respy -v -s -m"(not slow)" -x') == 0
-
-# TOX automation
-#os.system('pip install tox')
-#assert os.system('tox -v') == 0
