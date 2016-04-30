4703232c48934bccdb6cd26aac6ccc2f7c4228bd
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-04-30 16:51:36 +0200

    more regression tests

--- development/testing/tools/regression_vault.py
+++ development/testing/tools/regression_vault.py
@@ -7,72 +7,57 @@ regression tests. These are included in the test infrastructure.
 import pickle as pkl
 import numpy as np
 
-for idx in [2, 3]:
+import sys
+version = str(sys.version_info[0])
 
-    # doing execfile() on this file will alter the current interpreter's
-    # environment so you can import libraries in the virtualenv
-    if idx == 2:
-        activate_this_file = "/home/peisenha/.envs/restudToolbox2/bin/activate_this.py"
-    if idx == 3:
-        activate_this_file = "/home/peisenha/.envs/restudToolbox/bin/activate_this.py"
+if version == '2':
+    sys.path.insert(0, '../../../respy/tests')
+    from codes.random_init import generate_init
+else:
+    from respy.tests.codes.random_init import generate_init
 
+from respy.python.shared.shared_auxiliary import print_init_dict
 
-    execfile(activate_this_file, dict(__file__=activate_this_file))
+from respy.evaluate import evaluate
 
-    import sys
-    version = str(sys.version_info[0])
+from respy import RespyCls
+from respy import simulate
 
-    print(version)
+np.random.seed(213)
 
-#
-# if version == '2':
-#     sys.path.insert(0, '../../../respy/tests')
-#     from codes.random_init import generate_init
-# else:
-#     from respy.tests.codes.random_init import generate_init
-#
-# from respy.python.shared.shared_auxiliary import print_init_dict
-#
-# from respy.evaluate import evaluate
-#
-# from respy import RespyCls
-# from respy import simulate
-#
-# np.random.seed(213)
-#
-# num_tests = 100
-# fname = 'test_vault_' + version + '.respy.pkl'
-#
-# tests = []
-# for _ in range(num_tests):
-#
-#     init_dict = generate_init(constraints=None)
-#
-#     respy_obj = RespyCls('test.respy.ini')
-#
-#     simulate(respy_obj)
-#
-#     crit_val = evaluate(respy_obj)
-#
-#     test = (init_dict, crit_val)
-#
-#     tests += [test]
-#
-#     pkl.dump(tests, open(fname, 'wb'))
-#
-# print('.. done with creation.')
-#
-# # Now we make sure that the tests will pass.
-# tests = pkl.load(open(fname, 'rb'))
-#
-# for test in tests:
-#
-#     init_dict, crit_val = test
-#
-#     print_init_dict(init_dict)
-#
-#     respy_obj = RespyCls('test.respy.ini')
-#
-#     simulate(respy_obj)
-#
-#     np.testing.assert_almost_equal(evaluate(respy_obj), crit_val)
+num_tests = 100
+fname = 'test_vault_' + version + '.respy.pkl'
+
+tests = []
+for _ in range(num_tests):
+
+    init_dict = generate_init(constraints=None)
+
+    respy_obj = RespyCls('test.respy.ini')
+
+    simulate(respy_obj)
+
+    crit_val = evaluate(respy_obj)
+
+    test = (init_dict, crit_val)
+
+    tests += [test]
+
+    pkl.dump(tests, open(fname, 'wb'))
+
+print('.. done with creation.')
+
+# Now we make sure that the tests will pass.
+tests = pkl.load(open(fname, 'rb'))
+
+for test in tests:
+
+    init_dict, crit_val = test
+
+    print_init_dict(init_dict)
+
+    respy_obj = RespyCls('test.respy.ini')
+
+    simulate(respy_obj)
+
+    np.testing.assert_almost_equal(evaluate(respy_obj), crit_val)
