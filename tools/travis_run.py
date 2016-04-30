a532056634ec4255ee86fc93927601ece74b8117
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-04-30 22:08:30 +0200

    attempt at PYPI install

--- tools/travis_run.py
+++ tools/travis_run.py
@@ -4,9 +4,22 @@
 
 # standard library
 import subprocess as sp
+import numpy as np
+
+import os
 
 # Ensure that most recent installation of PIP is available
 sp.check_call('pip install --upgrade pip', shell=True)
+
+# Test the most recent PYPI submission. We need to switch to the directory to
+# for the site-packages to avoid the wrong import.
+cwd = os.getcwd()
+site_packages = np.__file__.replace('numpy/__init__.py', '')
+sp.check_call('pip install -vvv --no-binary respy respy', shell=True)
+os.chdir(site_packages)
+sp.check_call('python -c "import respy; respy.test()"', shell=True)
+sp.check_call('pip uninstall -y respy', shell=True)
+os.chdir(cwd)
 
 # Run PYTEST battery again on package in development mode. This ensure that the
 # current implementation is working properly.
