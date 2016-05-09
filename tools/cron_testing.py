26c8d77be41908b012dcae913f6c7c962464c19d
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-05-09 18:46:24 +0200

    edits

--- tools/cron_testing.py
+++ tools/cron_testing.py
@@ -9,7 +9,7 @@ import socket
 import os
 
 # Specify request
-HOURS, NOTIFICATION = 6, True
+HOURS, NOTIFICATION = 0.000001, True
 
 # Get some basic information about the system.
 HOSTNAME = socket.gethostname()
