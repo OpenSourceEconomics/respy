2259816cfbf1847e85edd615f9f48c66aa5c5d04
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-05-23 18:14:31 +0200

    all working like a charm

--- development/parallel/master.f90
+++ development/parallel/master.f90
@@ -23,6 +23,7 @@ PROGRAM master
     INTEGER(our_int), ALLOCATABLE   :: mapping_state_idx(:, :, :, :, :)
     INTEGER(our_int), ALLOCATABLE   :: states_all_tmp(:, :, :)
     INTEGER(our_int), ALLOCATABLE   :: states_number_period(:)
+    INTEGER(our_int), ALLOCATABLE   :: states_all(:, :, :)
 
     INTEGER(our_int)                :: status(MPI_STATUS_SIZE) 
     INTEGER(our_int)                :: max_states_period
@@ -43,6 +44,7 @@ PROGRAM master
     INTEGER(our_int)                :: ierr
     INTEGER(our_int)                :: task
 
+    REAL(our_dble), ALLOCATABLE     :: periods_payoffs_systematic(:, :, :)
     REAL(our_dble), ALLOCATABLE     :: periods_emax(:, :)
 
     REAL(our_dble)                  :: shocks_cholesky(4, 4)
@@ -50,6 +52,7 @@ PROGRAM master
     REAL(our_dble)                  :: coeffs_edu(3)
     REAL(our_dble)                  :: coeffs_a(6)
     REAL(our_dble)                  :: coeffs_b(6)
+    REAL(our_dble)                  :: crit_val
     REAL(our_dble)                  :: delta
     REAL(our_dble)                  :: tau
 
@@ -82,48 +85,76 @@ PROGRAM master
             is_interpolated, num_points, min_idx, request, num_draws_prob, & 
             is_myopic, tau)
 
-    ! Allocate arrays
-    ALLOCATE(mapping_state_idx(num_periods, num_periods, num_periods, min_idx, 2))
-    ALLOCATE(states_all_tmp(num_periods, 100000, 4))
-    ALLOCATE(states_number_period(num_periods))
 
-    CALL fort_create_state_space(states_all_tmp, states_number_period, &
-                mapping_state_idx, max_states_period, num_periods, edu_start, &
-                edu_max)
+    ! Execute on request.
+    IF (request == 'solve') THEN
 
-    ALLOCATE(periods_emax(num_periods, max_states_period))
+        ! Allocate arrays
+        ALLOCATE(mapping_state_idx(num_periods, num_periods, num_periods, min_idx, 2))
+        ALLOCATE(states_all_tmp(num_periods, 100000, 4))
+        ALLOCATE(states_number_period(num_periods))
+
+        CALL fort_create_state_space(states_all_tmp, states_number_period, &
+                    mapping_state_idx, max_states_period, num_periods, edu_start, &
+                    edu_max)
+
+        ALLOCATE(periods_emax(num_periods, max_states_period))
+
+        !TODO: Better
+        ! Calculate the systematic payoffs
+        ALLOCATE(states_all(num_periods, max_states_period, 4))
+        states_all = states_all_tmp(:, :max_states_period, :)
+        DEALLOCATE(states_all_tmp)
+
+        ALLOCATE(periods_payoffs_systematic(num_periods, max_states_period, 4))
+
+        CALL fort_calculate_payoffs_systematic(periods_payoffs_systematic, &
+                num_periods, states_number_period, states_all, edu_start, &
+                coeffs_a, coeffs_b, coeffs_edu, coeffs_home)
 
 
-    ! Spawn the slaves for doing the actual work.
-    CALL MPI_COMM_SPAWN('./slave', MPI_ARGV_NULL, num_slaves, MPI_INFO_NULL, & 
-            0, MPI_COMM_WORLD, SLAVECOMM, MPI_ERRCODES_IGNORE, ierr)
+        ! Spawn the slaves for doing the actual work.
+        CALL MPI_COMM_SPAWN('./slave', MPI_ARGV_NULL, num_slaves, MPI_INFO_NULL, & 
+                0, MPI_COMM_WORLD, SLAVECOMM, MPI_ERRCODES_IGNORE, ierr)
 
-    ! Request EMAX calculation
-    task = 2
-    CALL MPI_Bcast(task, 1, MPI_INT, MPI_ROOT, SLAVECOMM, ierr)
+        ! Request EMAX calculation
+        task = 2
+        CALL MPI_Bcast(task, 1, MPI_INT, MPI_ROOT, SLAVECOMM, ierr)
 
-    ! The leading slave is kind enough to let the parent process know about the 
-    ! intermediate outcomes.
-    DO period = (num_periods - 1), 0, -1
+        ! The leading slave is kind enough to let the parent process know about the 
+        ! intermediate outcomes.
+        DO period = (num_periods - 1), 0, -1
 
-        num_states = states_number_period(period + 1)
+            num_states = states_number_period(period + 1)
 
-        CALL MPI_RECV(periods_emax(period + 1, :num_states), num_states, & 
-                MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, SLAVECOMM, status, & 
-                ierr)
+            CALL MPI_RECV(periods_emax(period + 1, :num_states), num_states, & 
+                    MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, SLAVECOMM, status, & 
+                    ierr)
 
-    END DO
+        END DO
 
-    ! Shut down orderly
-    task = 1
-    CALL MPI_Bcast(task, 1, MPI_INT, MPI_ROOT, SLAVECOMM, ierr)
-    CALL MPI_FINALIZE (ierr)
+        ! Shut down orderly
+        task = 1
+        CALL MPI_Bcast(task, 1, MPI_INT, MPI_ROOT, SLAVECOMM, ierr)
+        CALL MPI_FINALIZE (ierr)
 
-    ! Write out result to allow temporary testing against the scalar implementation.
-    2500 FORMAT(1x,f25.15)
-    OPEN(UNIT=1, FILE='.eval.resfort.dat')
-    WRITE(1, 2500)  periods_emax(1, 1)
-    CLOSE(1)
+        ! Write out result to allow temporary testing against the scalar implementation.
+        2500 FORMAT(1x,f25.15)
+        OPEN(UNIT=1, FILE='.eval.resfort.dat')
+        WRITE(1, 2500)  periods_emax(1, 1)
+        CLOSE(1)
+
+
+    ELSE IF (request == 'evaluate') THEN
+
+    END IF
+
+    ! Store results. These are read in by the PYTHON wrapper and added to the 
+    ! RespyCls instance.
+    CALL store_results(mapping_state_idx, states_all, &
+            periods_payoffs_systematic, states_number_period, periods_emax, &
+            num_periods, min_idx, crit_val, request)
+
 
 END PROGRAM
 !*******************************************************************************
