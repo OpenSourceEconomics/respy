a47fb28d93c136278547e73832b9bde0b73faa6a
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-06-06 09:18:57 +0200

    removed lnsearch

--- development/optimizers/modified/bfgs/single/driver.py
+++ development/optimizers/modified/bfgs/single/driver.py
@@ -1,22 +1,28 @@
 
+import pickle as pkl
+
 import os
 
 
 from f2py_library import *
 
 import numpy as np
-np.random.seed(123)
 
-for _ in range(10):
+np.random.seed(123)#
 
+#rslt = []
+rslt = pkl.load(open('regresion_vault.pkl', 'rb'))
+
+for i in range(100):
+    print('Testing ', i)
     dim = np.random.randint(2, 10)    
 
     p_start = np.random.uniform(-0.1, 0.1, size=dim)
 
-    print(dim, p_start)
     fval, p_final = f2py_bfgs(p_start, dim)
 
-    print(fval, p_final)
+#    rslt += [[fval] + p_final.tolist()]
+    np.testing.assert_almost_equal(rslt[i], [fval] + p_final.tolist())
 
-    np.testing.assert_almost_equal(0, fval)
-
+# Store regression vault.
+#pkl.dump(rslt, open('regresion_vault.pkl', 'wb'))
