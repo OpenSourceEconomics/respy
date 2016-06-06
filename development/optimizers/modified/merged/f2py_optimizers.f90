d8e7993725d64f5d76ee59dbbcfa8e85d8760c71
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-06-06 17:16:30 +0200

    cleanup commit

---
+++ development/optimizers/modified/merged/f2py_optimizers.f90
@@ -0,0 +1,43 @@
+!******************************************************************************
+!******************************************************************************
+!   
+!   This module serves as the F2PY interface to the core functions. All the 
+!   functions have counterparts as PYTHON implementations.
+!
+!******************************************************************************
+!******************************************************************************
+SUBROUTINE f2py_newuoa(fval, p_final, p_start, func_dim_int)
+
+
+  USE newuoa_interface
+
+  INTEGER , INTENT(IN) :: func_dim_int
+
+  DOUBLE PRECISION, INTENT(IN)  :: p_start(func_dim_int)
+
+    DOUBLE PRECISION, INTENT(OUT)      :: fval
+
+  DOUBLE PRECISION, INTENT(OUT)  :: p_final(func_dim_int)
+
+  INTEGER:: NPT, IPRINT, MAXFUN
+
+  DOUBLE PRECISION :: RHOEND, RHOBEG
+
+   IPRINT=0
+      MAXFUN=50000
+      RHOBEG= MAXVAL(p_start)
+      RHOEND=1e-6 *RHOBEG
+
+
+    
+!    p_final = p_start
+  NPT=min(func_dim_int * 2, func_dim_int+2)
+
+CALL NEWUOA (p_final, NPT, RHOBEG, RHOEND, IPRINT, MAXFUN)   
+
+!fval = fret
+
+
+END SUBROUTINE
+!------------------------------------------------------------------------------
+!------------------------------------------------------------------------------
