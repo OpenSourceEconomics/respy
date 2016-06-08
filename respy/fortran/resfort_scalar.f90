e366a3ba55ccb2d669ff111ba866f1862c361126
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2016-06-08 08:00:20 +0200

    now removing F2PY

--- respy/fortran/resfort_scalar.f90
+++ respy/fortran/resfort_scalar.f90
@@ -17,45 +17,37 @@ PROGRAM resfort_scalar
     REAL(our_dble)                  :: coeffs_edu(3)
     REAL(our_dble)                  :: coeffs_a(6)
     REAL(our_dble)                  :: coeffs_b(6)
+    REAL(our_dble)                  :: x_start(26)
     REAL(our_dble)                  :: crit_val
 
-    REAL(our_dble)                  :: x_start(26)
-    
 !------------------------------------------------------------------------------
 ! Algorithm
 !------------------------------------------------------------------------------
 
-    ! Read specification of model. This is the FORTRAN replacement for the RespyCls instance that carries the model parametrization for the PYTHON/F2PY implementations.
+
     CALL read_specification(coeffs_a, coeffs_b, coeffs_edu, coeffs_home, shocks_cholesky)
 
-    ! This part creates (or reads from disk) the draws for the Monte Carlo integration of the EMAX. For is_debugging purposes, these might also be read in from disk or set to zero/one.   
     CALL create_draws(periods_draws_emax, num_draws_emax, seed_emax)
 
-    ! Execute on request.
+
     IF (request == 'solve') THEN
 
-        ! Solve the model for a given parametrization.    
         CALL fort_solve(periods_payoffs_systematic, states_number_period, mapping_state_idx, periods_emax, states_all, coeffs_a, coeffs_b, coeffs_edu, coeffs_home, shocks_cholesky, periods_draws_emax)
 
   ELSE IF (request == 'estimate') THEN
 
-        ! This part creates (or reads from disk) the draws for the Monte Carlo integration of the choice probabilities. For is_debugging purposes, these might also be read in from disk or set to zero/one.   
         CALL create_draws(periods_draws_prob, num_draws_prob, seed_prob)
 
-        ! Read observed dataset from disk.
         CALL read_dataset(data_array, num_agents_est)
 
-        ! Prepare interface for evaluation of criterion function
         CALL get_optim_paras(x_start, coeffs_a, coeffs_b, coeffs_edu, coeffs_home, shocks_cholesky)
 
-        ! Evaluate criterion function.
         crit_val = fort_criterion(x_start)
 
     END IF
 
-    ! Store results. These are read in by the PYTHON wrapper and added to the RespyCls instance.
-    CALL store_results(mapping_state_idx, states_all, periods_payoffs_systematic, states_number_period, periods_emax, crit_val)
 
+    CALL store_results(mapping_state_idx, states_all, periods_payoffs_systematic, states_number_period, periods_emax, crit_val)
 
 !******************************************************************************
 !******************************************************************************
