67900bc756f6d9c26741922d5cc67ad8de54eec0
peisenha <eisenhauer@policy-lab.org>
2016-08-02 23:35:05 +0200

    edits to infrasturcture as code

--- tools/terraform/respy.tf
+++ tools/terraform/respy.tf
@@ -5,6 +5,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "respy" {
-  ami           = "ami-4a8d1d5d"
+  ami           = "ami-6457c773"
   instance_type = "t2.micro"
+  key_name = "respy"
+  security_groups=["terraform_example"]
+    connection {
+        user = "ubuntu"
+        key_file = "/home/peisenha/.ssh/ec2-respy.pem"
+    }
+}
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+    name = "terraform_example"
+    description = "Used in the terraform"
+
+    # SSH access from anywhere
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
 }
