e5c67b543a4d44ef74408e61c3907ff934617a33
peisenha <eisenhauer@policy-lab.org>
2016-08-04 09:08:14 +0200

    edits

--- development/testing/_modules/auxiliary_scalability.py
+++ development/testing/_modules/auxiliary_scalability.py
@@ -1,9 +1,3 @@
-import matplotlib
-
-matplotlib.use('Agg')
-
-import matplotlib.pyplot as plt
-from datetime import timedelta
 from datetime import datetime
 import shlex
 import os
@@ -14,89 +8,22 @@ from config import SPEC_DIR
 import respy
 
 
-def get_durations():
-    rslt, labels, grid_slaves = dict(), [], []
+def get_actual_evaluations():
 
-    with open('scalability.respy.base', 'r') as infile:
+    with open('est.respy.info', 'r') as infile:
         for line in infile.readlines():
             list_ = shlex.split(line)
+
             if not list_:
                 continue
-            if list_[0] == 'Slaves':
+
+            if not len(list_) == 4:
                 continue
 
-            # Create key for each of the data specifications.
-            if 'kw_data' in list_[0]:
-                label = list_[0]
-                rslt[label] = dict()
-                labels += [label]
+            if list_[2] == 'Evaluations':
+                return int(list_[3])
 
-            # Process the interesting lines.
-            if len(list_) == 6:
-                num_slaves = int(list_[0])
-                grid_slaves += [num_slaves]
-                t = datetime.strptime(list_[5], "%H:%M:%S")
-                duration = timedelta(hours=t.hour, minutes=t.minute,
-                                     seconds=t.second)
-                rslt[label][num_slaves] = duration
-
-    # Remove all duplicates from the grid of slaves.
-    grid_slaves = sorted(list(set(grid_slaves)))
-
-    return rslt, labels, grid_slaves
-
-
-def linear_gains(num_slaves, ys):
-    if num_slaves == 0:
-        return ys[0]
-    else:
-        return ys[0] / num_slaves
-
-
-def plot_scalability():
-    rslt, labels, grid_slaves = get_durations()
-
-    ax = plt.figure(figsize=(12, 8)).add_subplot(111)
-
-    ys = []
-    for num_slaves in grid_slaves:
-        ys += [rslt[labels[0]][num_slaves].total_seconds()]
-
-    formatter = matplotlib.ticker.FuncFormatter(ylabel_formatting)
-
-    ax.plot(grid_slaves, ys, linewidth=5, label='RESPY Package',
-            color='red', alpha=0.8)
-
-    func_lin = []
-    for num_slaves in grid_slaves:
-        func_lin += [linear_gains(num_slaves, ys)]
-
-    ax.plot(grid_slaves, func_lin, linewidth=1, linestyle='--',
-            label='Linear Benchmark', color='black')
-
-    ax.yaxis.set_major_formatter(formatter)
-
-    ax.tick_params(labelsize=18, direction='out', axis='both', top='off',
-                   right='off')
-
-    ax.set_ylim([0.0, 6500])
-    ax.yaxis.get_major_ticks()[0].set_visible(False)
-    ax.set_ylabel('Hours', fontsize=16)
-
-    ax.set_xlabel('Number of Slaves', fontsize=16)
-    ax.set_xticks(grid_slaves)
-
-    # Set up legend
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.10),
-            fancybox=False, frameon=False, shadow=False,
-            ncol=2, fontsize=20)
-
-    plt.savefig('scalability.respy.png', bbox_inches='tight', format='png')
-
-
-def ylabel_formatting(x, _):
-    d = timedelta(seconds=x)
-    return str(d)
+    raise AssertionError
 
 
 def run(spec_dict, fname, grid_slaves):
@@ -138,31 +65,45 @@ def run(spec_dict, fname, grid_slaves):
         respy.estimate(respy_obj)
         finish_time = datetime.now()
 
+        if num_slaves == 0:
+            duration_baseline = finish_time - start_time
+            num_evals = get_actual_evaluations()
+
         os.chdir('../')
 
-        record_information(start_time, finish_time, num_slaves, maxfun)
+        record_information(start_time, finish_time, num_slaves, maxfun,
+                           duration_baseline, num_evals)
 
     os.chdir('../')
 
 
-def record_information(start_time, finish_time, num_slaves, maxfun):
-    fmt = '{:>15} {:>25} {:>25} {:>15}\n'
+def record_information(start_time, finish_time, num_slaves, maxfun,
+                       duration_baseline, num_evals):
+    fmt = '{:>15} {:>25} {:>25} {:>15} {:>15} {:>15}\n'
     if not os.path.exists('scalability.respy.info'):
         with open('scalability.respy.info', 'a') as out_file:
-            out_file.write('\n    Benchmarking ' + str(maxfun) +
-            ' evaluations\n\n')
+            out_file.write('\n    Benchmarking a maximum of ' + str(
+                maxfun) + ' evaluations\n\n')
             out_file.write(
-                fmt.format(*['Slaves', 'Start', 'Stop', 'Duration']))
+                fmt.format(*['Slaves', 'Start', 'Stop', 'Duration',
+                             'Benchmark', 'Evaluations']))
             out_file.write('\n')
 
     start_str = start_time.strftime("%Y-%m-%d %H:%M:%S")
     finish_str = finish_time.strftime("%Y-%m-%d %H:%M:%S")
 
     duration_time = finish_time - start_time
-    duration_str = strfdelta(duration_time, "{H:02}:{M:02}:{S:02}")
+    duration_actual_str = strfdelta(duration_time, "{H:02}:{M:02}:{S:02}")
+
+    duration_linear_str = '---'
+    if not num_slaves == 0:
+        duration_linear = duration_baseline / num_slaves
+        duration_linear_str = strfdelta(duration_linear, "{H:02}:{M:02}:{S:02}")
 
     with open('scalability.respy.info', 'a') as out_file:
-        line = [num_slaves, start_str, finish_str, duration_str]
+
+        line = [num_slaves, start_str, finish_str, duration_actual_str,
+                duration_linear_str, num_evals]
         out_file.write(fmt.format(*line))
 
 
--- development/testing/scalability/run.py
+++ development/testing/scalability/run.py
@@ -5,7 +5,6 @@ import sys
 
 sys.path.insert(0, '../_modules')
 from auxiliary_shared import aggregate_information
-from auxiliary_scalability import plot_scalability
 from auxiliary_shared import send_notification
 from auxiliary_shared import compile_package
 from auxiliary_scalability import run
@@ -13,12 +12,13 @@ from auxiliary_shared import cleanup
 from config import SPECS
 
 
-def check_scalability(args, GRID_SLAVES):
+def check_scalability(args, GRID_SLAVES, maxfun):
     """ Details of the scalability exercise can be specified in the code block
     below. Note that only deviations from the benchmark initialization files
     need to be addressed.
     """
     spec_dict = dict()
+    spec_dict['maxfun'] = maxfun
 
     if args.is_debug:
         GRID_SLAVES = [0, 2]
@@ -30,10 +30,6 @@ def check_scalability(args, GRID_SLAVES):
         spec_dict['scaling'] = [False, 0.00001]
         spec_dict['num_agents_sim'] = spec_dict['num_agents_est']
 
-    if args.is_finalize:
-        plot_scalability()
-        return
-
     cleanup()
 
     if args.is_compile:
@@ -43,7 +39,6 @@ def check_scalability(args, GRID_SLAVES):
         run(spec_dict, spec + '.ini', GRID_SLAVES)
 
     aggregate_information('scalability')
-    plot_scalability()
     send_notification('scalability')
 
 if __name__ == '__main__':
@@ -64,4 +59,6 @@ if __name__ == '__main__':
     parser.add_argument('--finalize', action='store_true', dest='is_finalize',
         default=False, help='just create graph')
 
-    check_scalability(parser.parse_args(), GRID_SLAVES)
+    maxfun = 1000
+
+    check_scalability(parser.parse_args(), GRID_SLAVES, maxfun)
