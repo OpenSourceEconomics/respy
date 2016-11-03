49f7019003b5f39b3021186d1a5748162fe305fc
peisenha <eisenhauer@policy-lab.org>
2016-11-03 11:18:38 +0100

    minor edits

--- respy/python/process/process_auxiliary.py
+++ respy/python/process/process_auxiliary.py
@@ -7,6 +7,7 @@ def check_dataset_est(data_frame, respy_obj):
     """ This routine runs some consistency checks on the simulated data frame.
     """
     # Distribute class attributes
+    num_agents_est = respy_obj.get_attr('num_agents_est')
     num_periods = respy_obj.get_attr('num_periods')
 
     # Check that there are not missing values in any of the columns but for
@@ -15,6 +16,12 @@ def check_dataset_est(data_frame, respy_obj):
         if label == 'Earnings':
             continue
         assert ~ data_frame[label].isnull().any()
+
+    # We want to make sure that the dataset contains exactly the number of
+    # agents that were requested. This might not necessarily be the case
+    # if a user requests an estimation with more agents than available.
+    dat = len(data_frame['Identifier'].unique())
+    np.testing.assert_equal(dat, num_agents_est)
 
     # Checks for PERIODS. It can happen that the last period is deleted for all
     # agents. Thus, this is not a strict equality for observed data. It is
