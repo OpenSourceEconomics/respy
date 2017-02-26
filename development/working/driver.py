e61fdfebffba984f2beae92f0313bf21500d393d
peisenha <eisenhauer@policy-lab.org>
2017-02-26 18:35:01 +0100

    bug hunting ctd.

--- development/working/driver.py
+++ development/working/driver.py
@@ -30,18 +30,24 @@ from respy import estimate
 
 from codes.random_init import generate_init
 
+from codes.auxiliary import write_draws
 
+np.random.seed(123)
 respy_obj = RespyCls('model.respy.ini')
 
-for num_procs in [1]:
+num_periods = respy_obj.get_attr('num_periods')
+max_draws = 354
+write_draws(num_periods, max_draws)
+
+for version in ['FORTRAN', 'PYTHON']:
+    print(version)
     respy_obj.reset()
-    print('going in with ', num_procs)
     respy_obj.unlock()
-    #respy_obj.attr['num_procs'] = num_procs
+    respy_obj.attr['version'] = version
     respy_obj.lock()
     simulate(respy_obj)
-    #_, crit = estimate(respy_obj)
-    #print(crit)
+    _, crit = estimate(respy_obj)
+    print(crit)
     #assert crit == 0.25443574243874
 
 
