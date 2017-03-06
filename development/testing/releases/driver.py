69b039f444a0de144d3c81fc5a245e7a4b7a0bd1
peisenha <eisenhauer@policy-lab.org>
2017-03-06 18:59:46 +0100

    adding some description for release testing

--- development/testing/releases/driver.py
+++ development/testing/releases/driver.py
@@ -136,6 +136,15 @@ def run(args):
 
 if __name__ == '__main__':
 
+    # NOTES:
+    #
+    #   Version    Description
+    #
+    #   2.0.4      Ambiguity, just added endogenous delta ...
+    #
+    #
+    #
+
     # The two releases that are tested against each other. These are
     # downloaded from PYPI in their own virtual environments.
     OLD_RELEASE, NEW_RELEASE = '1.0.00', '2.0.4'
