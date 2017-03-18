ac95e23db34a6cf75dff1e856c51ddee89cd65f0
peisenha <eisenhauer@policy-lab.org>
2017-03-18 17:17:24 +0100

    wrong order of labels in simulated

--- respy/python/shared/shared_constants.py
+++ respy/python/shared/shared_constants.py
@@ -59,13 +59,13 @@ DATA_LABELS_EST += ['Lagged_Schooling']
 
 # There is additional information available in a simulated dataset.
 DATA_LABELS_SIM = DATA_LABELS_EST[:]
+DATA_LABELS_SIM += ['Total_Reward_1', 'Total_Reward_2']
+DATA_LABELS_SIM += ['Total_Reward_3', 'Total_Reward_4']
 DATA_LABELS_SIM += ['Systematic_Reward_1', 'Systematic_Reward_2']
 DATA_LABELS_SIM += ['Systematic_Reward_3', 'Systematic_Reward_4']
 DATA_LABELS_SIM += ['Shock_Reward_1', 'Shock_Reward_2']
 DATA_LABELS_SIM += ['Shock_Reward_3', 'Shock_Reward_4']
-
-DATA_LABELS_SIM += ['Total_Reward_1', 'Total_Reward_2']
-DATA_LABELS_SIM += ['Total_Reward_3', 'Total_Reward_4', 'Discount_Rate']
+DATA_LABELS_SIM += ['Discount_Rate']
 
 DATA_FORMATS_EST = dict()
 for key_ in DATA_LABELS_EST:
