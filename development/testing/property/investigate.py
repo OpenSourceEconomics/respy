3e6f367d36ff6e389e99dade6453d019f58e1c84
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2017-06-17 07:50:48 +0200

    minor edits due to failed property test

--- development/testing/property/investigate.py
+++ development/testing/property/investigate.py
@@ -45,12 +45,12 @@ np.random.seed(seed)
 test_dict = get_test_dict(PACKAGE_DIR + '/respy/tests')
 module, method = get_random_request(test_dict)
 
-module, method = 'test_integration', 'test_8'
+module, method = 'test_versions', 'test_4'
 count = 0
 #os.system('git clean -d -f')
 for i in range(100):
 
-    #seed = 3829
+    seed = 95508
     seed = i + 3825
     #seed = 38280
     np.random.seed(seed)
@@ -66,4 +66,4 @@ for i in range(100):
     #count = count +1
     #print('completed ', count)
 
-    os.system('git clean -d -f')
+    #os.system('git clean -d -f')
