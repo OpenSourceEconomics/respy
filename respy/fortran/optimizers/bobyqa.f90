5a725adc872e1ea0418e99e5c19fc5e3a594a9ac
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2017-07-24 10:49:24 +0200

    larger aux W in bobyqua

--- respy/fortran/optimizers/bobyqa.f90
+++ respy/fortran/optimizers/bobyqa.f90
@@ -47,7 +47,7 @@ SUBROUTINE bobyqa(FUNC, X, NPT, RHOBEG, RHOEND, IPRINT, MAXFUN, SUCCESS, MESSAGE
 
     REAL(our_dble)                  :: XL(SIZE(X))
     REAL(our_dble)                  :: XU(SIZE(X))
-    REAL(our_dble)                  :: W(20000)
+    REAL(our_dble)                  :: W(200000)
 
     INTEGER(our_int)                :: N
 
