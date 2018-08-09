910e777f724311017fb5c58e3b2012025b3b55e0
Janos Gabler <janos.gabler@gmail.com>
2018-08-09 10:33:17 +0200

    Pulled out some functions from model_processing into model_processing_auxiliary.

--- respy/pre_processing/model_processing.py
+++ respy/pre_processing/model_processing.py
@@ -1,6 +1,14 @@
 """Process a Respy initialization file.
 
+The module contains tools to read and write initialization files and to
+convert between two different dictionary representations of a model:
 
+1) init_dict: a dictionary that contains all of the information from the .ini file
+in a structure that is very close to that file.
+
+2) attr_dict: a dictionary that contains the information from the .ini file and
+additional derived information in a structure that is helpful for further
+usage in RespyCLS.
 
 """
 import numpy as np
@@ -9,6 +17,9 @@ from respy.python.shared.shared_constants import OPT_EST_FORT
 from respy.python.shared.shared_constants import OPT_EST_PYTH
 from respy.python.shared.shared_auxiliary import cholesky_to_coeffs
 from respy.python.shared.shared_auxiliary import format_opt_parameters
+from respy.pre_processing.model_processing_auxiliary import \
+    _process_coefficient_line, _type_conversions, _add_to_dictionary, \
+    _determine_case, _paras_mapping
 
 
 def read(fname):
@@ -17,7 +28,7 @@ def read(fname):
     The design of the initialization files is optimized for readability and user
     friendliness, not developer friendliness.
 
-    The resulting dictionary is still very close to the structure of the ini
+    The resulting dictionary is very close to the structure of the ini
     file and will be processed later.
 
     """
@@ -43,7 +54,7 @@ def read(fname):
             is_fixed, bounds = None, None
 
             if flag in ['coeff']:
-                is_fixed, bounds = process_coefficient_line(line)
+                is_fixed, bounds = _process_coefficient_line(line)
 
             value = _type_conversions(flag, value)
 
@@ -487,125 +498,3 @@ def default_model_dict():
     }
 
     return default
-
-
-def process_coefficient_line(line):
-    """Extract the information about the estimation."""
-    def _process_bounds(line):
-
-        start, end = line.find('(') + 1, line.find(')')
-        bounds = line[start:end].split(',')
-        for i in range(2):
-            if bounds[i].upper() == 'NONE':
-                bounds[i] = None
-            else:
-                bounds[i] = float(bounds[i])
-
-        return bounds
-
-    list_ = shlex.split(line)
-
-    # The coefficient is neither fixed nor are any bounds specified.
-    if len(list_) == 2:
-        is_fixed, bounds = False, [None, None]
-    # The coefficient is both fixed and bounds are specified.
-    elif len(list_) == 4:
-        is_fixed, bounds = True, _process_bounds(line)
-    # The coefficient is only fixed.
-    elif (len(list_) == 3) and (list_[2] == '!'):
-        is_fixed, bounds = True, [None, None]
-    # The coefficient has only bounds
-    elif len(list_) == 3 and (list_[2][0] == '('):
-        is_fixed, bounds = False, _process_bounds(line)
-    else:
-        raise AssertionError
-
-    return is_fixed, bounds
-
-
-def _type_conversions(flag, value):
-    """Convert the type of a value according to its flag."""
-    # Type conversion
-    if flag in ['agents', 'periods', 'start', 'max', 'draws', 'seed', 'points',
-                'maxiter', 'maxfun', 'procs', 'npt', 'maxiter', 'm', 'maxls']:
-        value = int(value)
-    elif flag in ['file', 'options', 'type']:
-        value = str(value)
-    elif flag in ['debug', 'store', 'flag', 'mean']:
-        assert (value.upper() in ['TRUE', 'FALSE'])
-        value = (value.upper() == 'TRUE')
-    elif flag in ['version', 'optimizer']:
-        value = value.upper()
-    else:
-        value = float(value)
-
-    # Finishing
-    return value
-
-
-def _add_to_dictionary(group, flag, value, is_fixed, bounds, dict_):
-    """Process most parts of the initialization file."""
-    # This aligns the label from the initialization file with the label inside
-    # the RESPY logic.
-    if flag in ['coeff', 'shift'] or (group == 'TYPES' and flag == 'share'):
-        flag = 'coeffs'
-
-    # Prepare container for information about coefficients
-    if ('coeffs' not in dict_[group].keys()) and (flag in ['coeffs']):
-        dict_[group]['coeffs'] = []
-        dict_[group]['bounds'] = []
-        dict_[group]['fixed'] = []
-    elif group == 'EDUCATION' and flag not in dict_[group].keys():
-        dict_[group][flag] = []
-
-    # Collect information
-    if flag in ['coeffs']:
-        dict_[group]['coeffs'] += [value]
-        dict_[group]['bounds'] += [bounds]
-        dict_[group]['fixed'] += [is_fixed]
-    elif group == 'EDUCATION' and (flag in ['share', 'start', 'lagged']):
-        dict_[group][flag] += [value]
-    elif flag not in ['shift'] or (group == 'TYPES' and flag == 'share'):
-        dict_[group][flag] = value
-
-    # Finishing.
-    return dict_
-
-
-def _determine_case(list_):
-    """Process cases and determine whether group flag or empty line."""
-    # Antibugging
-    assert (isinstance(list_, list))
-
-    # Get information
-    is_empty = (len(list_) == 0)
-
-    if not is_empty:
-        is_group = list_[0].isupper()
-        is_comment = list_[0] == '#'
-    else:
-        is_group = False
-        is_comment = False
-
-    # Antibugging
-    assert is_group in [True, False]
-    assert is_empty in [True, False]
-    assert is_comment in [True, False]
-
-    # Finishing
-    return is_empty, is_group, is_comment
-
-
-def _paras_mapping():
-    """Mapping to re-sort parameters.
-
-    We need to do some reorganization as the parameters from the
-    initialization that describe the covariance of the shocks need to be
-    mapped to the Cholesky factors which are the parameters the optimizer
-    actually iterates on.
-
-    """
-    mapping = [
-        (43, 43), (44, 44), (45, 46), (46, 49), (47, 45), (48, 47),
-        (49, 50), (50, 48), (51, 51), (52, 52)]
-    return mapping
