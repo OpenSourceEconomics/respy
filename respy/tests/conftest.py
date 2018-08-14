751db1e72a82637aba5821ada35e85f7574ad48e
Janos Gabler <janos.gabler@gmail.com>
2018-08-14 11:51:44 +0200

    Removed pytest.ini after changing conftest.py.

--- respy/tests/conftest.py
+++ respy/tests/conftest.py
@@ -7,14 +7,14 @@ import pytest
 import os
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='function', autouse=True)
 def set_seed():
     """ Each test is executed with the same random seed.
     """
     np.random.seed(1423)
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='function', autouse=True)
 def fresh_directory():
     """ Each test is executed in a fresh directory.
     """
