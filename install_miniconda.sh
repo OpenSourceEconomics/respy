a7a73f7daf5f75027dd6bd5685754a7233def00e
Tobias Raabe <tobiasraabe@uni-bonn.de>
2019-03-04 16:10:51 +0100

    Fix travis setup.

---
+++ install_miniconda.sh
@@ -0,0 +1,11 @@
+set -e
+
+if [[ "$TRAVIS_PYTHON_VERSION" == "3.7-dev" ]]; then
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+else
+  wget https://repo.continuum.io/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh -O miniconda.sh;
+fi
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda config --set always_yes yes --set changeps1 no
+conda info -a
