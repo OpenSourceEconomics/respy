9a67f2a85ea3119e1840a43f8d1e06b375432920
Tobias Raabe <tobiasraabe@uni-bonn.de>
2019-03-04 17:30:56 +0100

    fix.

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -6,6 +6,8 @@ else
   wget https://repo.continuum.io/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh -O miniconda.sh;
 fi
 bash miniconda.sh -b -p /home/travis/miniconda
+ls /home/travis
+ls /home/travis/miniconda
 export PATH=/home/travis/miniconda/bin:$PATH
 conda config --set always_yes yes --set changeps1 no
 conda info -a
