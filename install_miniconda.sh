c6cad951f6e46126d8e2a7fe563d1a3bf924da6a
Tobias Raabe <tobiasraabe@uni-bonn.de>
2019-03-04 17:25:14 +0100

    edit.

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -5,7 +5,7 @@ if [[ "$TRAVIS_PYTHON_VERSION" == "3.7-dev" ]]; then
 else
   wget https://repo.continuum.io/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh -O miniconda.sh;
 fi
-miniconda.sh -b
+bash miniconda.sh -b -p /home/travis/miniconda
 export PATH=/home/travis/miniconda/bin:$PATH
 conda config --set always_yes yes --set changeps1 no
 conda info -a
