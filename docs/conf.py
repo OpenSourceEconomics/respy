fe716a2b2ef4f1f117a8361a1f479e30b45e8713
Tobias Raabe <tobiasraabe@uni-bonn.de>
2019-04-25 00:08:42 +0200

    use index.rst.

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,8 @@ release = "1.2.0"
 
 # -- General configuration ------------------------------------------------
 
+master_doc = "index"
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
