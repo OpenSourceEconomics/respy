989fdbc4c99a81226b6f4406150e05676edfe2df
Tobias Raabe <tobiasraabe@uni-bonn.de>
2019-05-03 20:34:45 +0200

    Add xfails to let 10 regression tests run.

--- respy/tests/test_regression.py
+++ respy/tests/test_regression.py
@@ -12,13 +12,28 @@ from respy.python.shared.shared_constants import TOL
 from respy.tests.codes.auxiliary import simulate_observed
 
 
-@pytest.mark.parametrize("index", range(7))
+params_fortran = [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9]
+
+params_python = [
+    0,
+    1,
+    2,
+    pytest.param(3, marks=pytest.mark.xfail),
+    pytest.param(4, marks=pytest.mark.xfail),
+    pytest.param(5, marks=pytest.mark.xfail),
+    6,
+    pytest.param(7, marks=pytest.mark.xfail),
+    pytest.param(8, marks=pytest.mark.xfail),
+    pytest.param(9, marks=pytest.mark.xfail),
+]
+
+params = params_fortran if IS_FORTRAN else params_python
+
+
+@pytest.mark.parametrize("index", params)
 def test_single_regression(regression_vault, index):
     """Run a single regression test."""
     attr, crit_val = regression_vault[index]
-
-    if not IS_FORTRAN and attr["version"] == "fortran":
-        pytest.skip()
 
     if not IS_PARALLELISM_OMP or not IS_FORTRAN:
         attr["num_threads"] = 1
