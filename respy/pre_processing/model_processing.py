00e4ec52c0b47b9adef3ad21ea0cfb3b3a6c40e8
Janos Gabler <janos.gabler@gmail.com>
2019-05-30 09:57:26 +0200

    Remove more code in model_processing.py.

--- respy/pre_processing/model_processing.py
+++ respy/pre_processing/model_processing.py
@@ -36,16 +36,6 @@ def _create_attribute_dictionary(options_spec):
     return attr
 
 
-def _get_num_types(params_spec):
-    if "type_shares" in params_spec.index:
-        len_type_shares = len(params_spec.loc["type_shares"])
-        num_types = len_type_shares / 2 + 1
-    else:
-        num_types = 1
-
-    return num_types
-
-
 def _read_params_spec(input_):
     if not isinstance(input_, (Path, pd.DataFrame)):
         raise TypeError("params_spec must be Path or pd.DataFrame.")
@@ -100,114 +90,39 @@ def parse_parameters(params, paras_type="optim"):
 
     """
     if isinstance(params, pd.DataFrame):
-        paras_vec = params["para"].to_numpy()
+        params = params["para"]
     elif isinstance(params, pd.Series):
-        paras_vec = params.to_numpy()
+        pass
     else:
         raise ValueError("Invalid params in parse_parameters: {}.".format(type(params)))
 
-    pinfo = _paras_parsing_information(len(paras_vec))
     optim_paras = {}
 
-    # basic extraction
-    for quantity in pinfo:
-        start = pinfo[quantity]["start"]
-        stop = pinfo[quantity]["stop"]
-        optim_paras[quantity] = paras_vec[start:stop]
+    for quantity in params.index.get_level_values("category").unique():
+        optim_paras[quantity] = params.loc[quantity].to_numpy()
 
-    # modify the shock_coeffs
-    if paras_type == "econ":
-        shocks_cholesky = _coeffs_to_cholesky(optim_paras["shocks_coeffs"])
-    else:
-        shocks_cholesky = _extract_cholesky(paras_vec)
+    shocks = optim_paras["shocks"]
+    dim = _get_matrix_dimension_from_num_triangular_elements(len(shocks))
+    shocks_cholesky = np.zeros((dim, dim))
+    shocks_cholesky[np.tril_indices(dim)] = shocks
     optim_paras["shocks_cholesky"] = shocks_cholesky
-    del optim_paras["shocks_coeffs"]
 
-    # overwrite the type information
-    type_shares, type_shifts = _extract_type_information(paras_vec)
-    optim_paras["type_shares"] = type_shares
-    optim_paras["type_shifts"] = type_shifts
-    optim_paras["num_paras"] = paras_vec.shape[0]
-    optim_paras["num_types"] = type_shifts.shape[0]
+    if "type_shares" in optim_paras:
+        optim_paras["type_shares"] = np.hstack(
+            [np.zeros(2), optim_paras["type_shares"]]
+        )
+        optim_paras["type_shifts"] = np.vstack(
+            [np.zeros(4), optim_paras["type_shift"].reshape(-1, 4)]
+        )
+        optim_paras["num_types"] = optim_paras["type_shifts"].shape[0]
+    else:
+        optim_paras["num_types"] = 1
+        optim_paras["type_shares"] = np.zeros(2)
+        optim_paras["type_shifts"] = np.zeros((1, 4))
+
+    optim_paras["num_paras"] = len(params)
 
     return optim_paras
-
-
-def _extract_type_information(x):
-    """Extract the information about types from a parameter vector of type 'optim'."""
-    pinfo = _paras_parsing_information(len(x))
-
-    # Type shares
-    start, stop = pinfo["type_shares"]["start"], pinfo["type_shares"]["stop"]
-    num_types = int(len(x[start:]) / 6) + 1
-    type_shares = x[start:stop]
-    type_shares = np.hstack((np.zeros(2), type_shares))
-
-    # Type shifts
-    start, stop = pinfo["type_shifts"]["start"], pinfo["type_shifts"]["stop"]
-    type_shifts = x[start:stop]
-    type_shifts = np.reshape(type_shifts, (num_types - 1, 4))
-    type_shifts = np.vstack((np.zeros(4), type_shifts))
-
-    return type_shares, type_shifts
-
-
-def _extract_cholesky(x):
-    """Extract the Cholesky factor from the shock's covariance matrix."""
-    pinfo = _paras_parsing_information(len(x))
-    start, stop = (pinfo["shocks_coeffs"]["start"], pinfo["shocks_coeffs"]["stop"])
-    shocks_coeffs = x[start:stop]
-
-    dim = _get_matrix_dimension_from_num_triangular_elements(len(shocks_coeffs))
-    shocks_cholesky = np.zeros((dim, dim))
-    shocks_cholesky[np.tril_indices(dim)] = shocks_coeffs
-
-    return shocks_cholesky
-
-
-def _coeffs_to_cholesky(coeffs):
-    """Return the cholesky factor of a covariance matrix described by coeffs.
-
-    The function can handle the case of a deterministic model where all coefficients
-    were zero.
-
-    Parameters
-    ----------
-    coeffs : np.ndarray
-        Array with shape (num_coeffs,) that contains the upper triangular elements of a
-        covariance matrix whose diagonal elements have been replaced by their square
-        roots.
-
-    """
-    dim = _get_matrix_dimension_from_num_triangular_elements(coeffs.shape[0])
-    shocks = np.zeros((dim, dim))
-    shocks[np.triu_indices(dim)] = coeffs
-    shocks[np.diag_indices(dim)] **= 2
-
-    shocks_cov = shocks + shocks.T - np.diag(shocks.diagonal())
-
-    if np.count_nonzero(shocks_cov) == 0:
-        return np.zeros((dim, dim))
-    else:
-        return np.linalg.cholesky(shocks_cov)
-
-
-def _paras_parsing_information(num_paras):
-    """Dictionary with the start and stop indices of each quantity."""
-    num_types = int((num_paras - 53) / 6) + 1
-    num_shares = (num_types - 1) * 2
-    pinfo = {
-        "delta": {"start": 0, "stop": 1},
-        "coeffs_common": {"start": 1, "stop": 3},
-        "coeffs_a": {"start": 3, "stop": 18},
-        "coeffs_b": {"start": 18, "stop": 33},
-        "coeffs_edu": {"start": 33, "stop": 40},
-        "coeffs_home": {"start": 40, "stop": 43},
-        "shocks_coeffs": {"start": 43, "stop": 53},
-        "type_shares": {"start": 53, "stop": 53 + num_shares},
-        "type_shifts": {"start": 53 + num_shares, "stop": num_paras},
-    }
-    return pinfo
 
 
 def _get_matrix_dimension_from_num_triangular_elements(num):
