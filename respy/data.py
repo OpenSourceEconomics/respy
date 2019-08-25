e9185f5c78c0cc4dfe1da53e0dacc1820dc8042c
Tobias Raabe <tobiasraabe@uni-bonn.de>
2019-08-25 07:52:37 +0200

    Fixed data.

--- respy/data.py
+++ respy/data.py
@@ -57,42 +57,10 @@ def create_kw_97(params, options):
 
     df = _create_working_experience(df, options)
 
-    df["Lagged_Choice"] = df.groupby("Identifier").Choice.shift(1)
+    df["Lagged_Choice_1"] = df.groupby("Identifier").Choice.shift(1)
 
     labels, _ = rp_shared.generate_column_labels_estimation(options)
 
     df = df.assign(Period=df.Age - 16).drop(columns="Age").loc[:, labels]
-
-    return df
-
-
-def create_kw_94(params, options):
-    """Convert data to be used with Keane and Wolpin (1994).
-
-    The model in Keane and Wolpin (1994) does not include military as an occupational
-    choice. Thus, the history of individuals is truncated the first time they enter the
-    military.
-
-    """
-    _, _, options = process_params_and_options(params, options)
-
-    df = create_kw_97(options)
-    # Create indicator if an individual entered the military and from thereon.
-    df["Entered_Military"] = df.Choice.eq("mil")
-    df.Entered_Military = df.groupby("Identifier").Entered_Military.cumsum()
-
-    df = df.loc[df.Entered_Military.eq(0)].copy()
-    df = df.drop(columns=["Entered_Military", "Experience_Mil"])
-
-    # Remove military as choice.
-    df.Choice = df.Choice.cat.remove_unused_categories()
-    df.Lagged_Choice = df.Lagged_Choice.cat.remove_unused_categories()
-
-    labels, _ = rp_shared.generate_column_labels_estimation(options)
-
-    df = df.reset_index(drop=True).loc[:, labels]
-
-    # reset the Identifier such that it starts at 0 and always increments by 1
-    df["Identifier"] = pd.Categorical(df["Identifier"]).codes
 
     return df
