693b514ae56b23446e1adae3005d3d5c00768702
tobiasstenzel <tobias.stenzel@mailbox.org>
2019-09-02 14:38:26 +0200

    implemented csv use for replication test

--- respy/tests/test_replication_kw_94.py
+++ respy/tests/test_replication_kw_94.py
@@ -14,11 +14,11 @@ def test_table_6_exact_solution_row_mean_and_sd():
     500, 1000, and 2000 dollar tuition fee on years of schooling and of
     experience in occupation 1 and occupation 2 based on 40 samples of
     100 persons using true paramter values are tested.
-
+    
     """
-
+    
     # Initialize the respective simulate function.
-    params, options, _ = rp.get_example_model("kw_94_one", with_data=False)
+    params, options, _ = rp.get_example_model("kw_94_one")
     options["simulation_agents"] = 4000
     simulate = rp.get_simulate_func(params, options)
     
@@ -30,10 +30,10 @@ def test_table_6_exact_solution_row_mean_and_sd():
     # respective tuition subsidy.
     data_frames = []
     for model, tuition_subsidy in zip(models, tuition_subsidies):
-        params, _, _ = rp.get_example_model(f"kw_94_{model}", with_data=False)
+        params, _, _ = rp.get_example_model(f"kw_94_{model}")
         params.loc[("nonpec_edu", "hs_graduate"), "value"] += tuition_subsidy
         data_frames.append(simulate(params))
-
+    
     # Calculate the statistics based on 40 bootstrap samples á 100 agents.
     bootstrapped_statistics = []
     for i, title in zip(range(0, 6, 2), ["Data Set One", "Data Set Two", "Data Set Three"]):
@@ -50,9 +50,9 @@ def test_table_6_exact_solution_row_mean_and_sd():
     
     rp_replication = pd.concat([bs.agg(["mean", "std"]) for bs in bootstrapped_statistics], axis=1)
     
-    # Expected values are copied from page 668 in Keane/Wolpin(1994).
+    # Expected values are taken from csv in ..\resources.
     kw = pd.DataFrame(rp_replication, copy=True)
-    kw.loc['mean'] = [1.44, -3.43, 2.19, 1.12, -2.71, 2.08, 1.67, -1.27, -0.236]
-    kw.loc['std'] = [0.18, 0.94, 0.89, 0.22, 0.53, 0.43, 0.20, 0.18, 0.10]
+    kw_94_table_6 = pd.read_csv(r'resources/kw_94_table_6.csv', sep=";")
+    kw.loc[['mean', 'std'],:] = kw_94_table_6.iloc[1:3,1:].astype(float).to_numpy()
     
     np.testing.assert_allclose(rp_replication, kw, rtol=0.02, atol=0)
