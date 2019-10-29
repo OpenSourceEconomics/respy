Wrong order of calculations.

respy/shared.py
@@ -10,7 +10,8 @@ from numba import njit
 from numba import vectorize
 
 from respy.config import INADMISSIBILITY_PENALTY
-from respy.config import MAX_FLOAT
+from respy.config import MAX_LOG_FLOAT
+from respy.config import MIN_LOG_FLOAT
 
 
 @njit
@@ -61,8 +62,8 @@ def transform_disturbances(draws, shocks_mean, shocks_cholesky, n_wages):
 
     draws_transformed += shocks_mean
 
-    draws_transformed[:, :n_wages] = np.clip(
-        np.exp(draws_transformed[:, :n_wages]), 0, MAX_FLOAT
+    draws_transformed[:, :n_wages] = np.exp(
+        np.clip(draws_transformed[:, :n_wages], MIN_LOG_FLOAT, MAX_LOG_FLOAT)
     )
 
     return draws_transformed