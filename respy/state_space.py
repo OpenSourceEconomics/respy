e31ccd18f9382cae7f9a1ab256128ccf320ef955
Tobias Raabe <tobiasraabe@uni-bonn.de>
2020-02-17 09:17:59 +0100

    Stay serial.

--- respy/state_space.py
+++ respy/state_space.py
@@ -1,8 +1,6 @@
 """Everything related to the state space of a structural model."""
 import itertools
-from pathlib import Path
 
-import joblib
 import numba as nb
 import numpy as np
 import pandas as pd
@@ -301,9 +299,6 @@ class _SingleDimStateSpace(_BaseStateSpace):
         n_choices_w_wage = len(optim_paras["choices_w_wage"])
         n_choices_wo_wage = n_choices - n_choices_w_wage
 
-        base_path = Path.cwd() / ".respy"
-        base_path.mkdir(parents=True, exist_ok=True)
-
         for name, array in (
             ("expected_value_functions", np.empty(n_states)),
             (
@@ -317,14 +312,6 @@ class _SingleDimStateSpace(_BaseStateSpace):
             ),
             ("nonpecs", np.zeros((n_states, n_choices))),
         ):
-            if self.dense_dim:
-                dense_dim_to_string = "_".join(map(str, self.dense_dim))
-                path = base_path / f"{name}_{dense_dim_to_string}.npy"
-                if path.exists():
-                    path.unlink()
-                joblib.dump(array, path)
-                array = joblib.load(path, mmap_mode="r+")
-
             setattr(self, name, array)
 
 
