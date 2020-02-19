53888d874d1a579bfacc47e57a4ba28924a8b65c
Tobias Raabe <tobiasraabe@uni-bonn.de>
2020-02-19 12:19:58 +0100

    Better parsing for observables (and exog procs). Only allow observables with at least two values.

---
+++ respy/tests/test_process_covariates.py
@@ -0,0 +1,48 @@
+import io
+from textwrap import dedent
+
+import pandas as pd
+
+from respy.pre_processing.process_covariates import remove_irrelevant_covariates
+
+
+def test_identify_relevant_covariates():
+    params = pd.read_csv(
+        io.StringIO(
+            dedent(
+                """
+                category,name,value
+                wage_a,constant,1
+                nonpec_b,upper_upper,1
+                wage_c,upper_upper_with_spacing_problem,1
+                """
+            )
+        ),
+        index_col=["category", "name"],
+    )
+
+    options = {
+        "covariates": {
+            "constant": "1",
+            "nested_covariate": "2",
+            "upper": "nested_covariate > 2",
+            "upper_upper": "upper == 5",
+            "unrelated_covariate": "2",
+            "unrelated_covariate_upper": "unrelated_covariate",
+            "upper_upper_with_spacing_problem": "upper>2",
+        }
+    }
+
+    relevant_covariates = remove_irrelevant_covariates(options, params)
+
+    expected = {
+        "covariates": {
+            "constant": "1",
+            "nested_covariate": "2",
+            "upper": "nested_covariate > 2",
+            "upper_upper": "upper == 5",
+            "upper_upper_with_spacing_problem": "upper>2",
+        }
+    }
+
+    assert expected == relevant_covariates
