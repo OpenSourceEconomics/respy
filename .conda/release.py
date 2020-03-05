61b92057d7cb0b0d3d525fd38abebc8d266ba3de
Tobias Raabe <tobiasraabe@uni-bonn.de>
2020-03-05 15:31:23 +0100

    Fix.

--- .conda/release.py
+++ .conda/release.py
@@ -18,11 +18,13 @@ from conda_build.api import convert
 
 CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 
-TEMPORARY_FOLDERS = [
-    Path("..", ".coverage"),
-    Path("..", "docs", "_build"),
-    Path("..", "docs", "_generated"),
-] + list(Path("..").glob("**/__pycache__"))
+FILE_LOCATION = Path(__file__).parent
+
+TEMPORARY_FILES_AND_FOLDERS = [
+    Path(FILE_LOCATION, "..", ".coverage"),
+    Path(FILE_LOCATION, "..", "docs", "_build"),
+    Path(FILE_LOCATION, "..", "docs", "_generated"),
+] + list(Path(FILE_LOCATION, "..").glob("**/__pycache__"))
 
 
 @click.group(context_settings=CONTEXT_SETTINGS, chain=True, invoke_without_command=True)
@@ -44,16 +46,17 @@ def clean():
     )
 
     # Check for environments in .tox.
-    tox_envs = list(Path("..", ".tox").glob("*"))
+    tox_directory = Path(FILE_LOCATION, "..", ".tox")
+    tox_envs = list(tox_directory.glob("*"))
     if tox_envs and click.confirm(
         "Do you want to remove all tests environments under .tox?"
     ):
         for path in tox_envs:
             subprocess.run(f"conda env remove -p {path}", shell=True)
-        shutil.rmtree(Path("..", ".tox"))
+        shutil.rmtree(tox_directory)
 
     # Check for temporary files and folders which can be deleted.
-    for path in TEMPORARY_FOLDERS:
+    for path in TEMPORARY_FILES_AND_FOLDERS:
         if path.is_dir():
             shutil.rmtree(path)
         elif path.is_file():
@@ -82,7 +85,7 @@ def clean():
 def build_convert_upload():
     """Build, convert and upload the package to anaconda.org."""
     platforms = ["osx-64", "linux-32", "linux-64", "win-32", "win-64"]
-    built_packages = build(".", need_source_download=False)
+    built_packages = build(str(FILE_LOCATION.parent), need_source_download=False)
     converted_packages = []
 
     for path in built_packages:
