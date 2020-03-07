4ad2b4f33ba9bf243fd2e5f173fda25376148a85
Tobias Raabe <tobiasraabe@uni-bonn.de>
2020-03-07 13:13:40 +0100

    Remove test.

--- respy/tests/test_interface.py
+++ respy/tests/test_interface.py
@@ -1,13 +1,8 @@
 import pytest
 
-from respy import test
 from respy.config import EXAMPLE_MODELS
 from respy.interface import get_example_model
 from respy.interface import get_parameter_constraints
-
-
-def test_internal_test_command():
-    test("--collect-only")
 
 
 @pytest.mark.parametrize("model", EXAMPLE_MODELS)
@@ -17,4 +12,4 @@ def test_get_example_model(model):
 
 @pytest.mark.parametrize("model", EXAMPLE_MODELS)
 def test_get_parameter_constraints(model):
-    assert get_parameter_constraints(model)
+    get_parameter_constraints(model)
