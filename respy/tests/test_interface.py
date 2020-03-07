b034bb162964642da40d45be6e846cbb0d818899
Tobias Raabe <tobiasraabe@uni-bonn.de>
2020-03-07 09:25:53 +0100

    Added test.

--- respy/tests/test_interface.py
+++ respy/tests/test_interface.py
@@ -1,8 +1,13 @@
 import pytest
 
+from respy import test
 from respy.config import EXAMPLE_MODELS
 from respy.interface import get_example_model
 from respy.interface import get_parameter_constraints
+
+
+def test_internal_test_command():
+    test("--collect-only")
 
 
 @pytest.mark.parametrize("model", EXAMPLE_MODELS)
