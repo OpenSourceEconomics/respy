e0538c399d3db24448dd2e5bad56608725e0b7a4
mo2561057 <44094181+mo2561057@users.noreply.github.com>
2020-04-24 00:23:47 +0200

    Update respy/state_space.py
    Co-Authored-By: Tobias Raabe <raabe@posteo.de>

--- respy/state_space.py
+++ respy/state_space.py
@@ -41,9 +41,7 @@ def create_state_space_class(options, optim_paras):
 
     # I think here we can get more elegant! Or is this the only way?
     core_index_to_complex = {i: k for i, k in enumerate(core_period_choice)}
-    core_index_to_indices = {
-        i: core_period_choice[core_index_to_complex[i]] for i in core_index_to_complex
-    }
+    core_index_to_indices = {k: core_period_choice[v] for k, v in core_index_to_complex.items()}
 
     # Create sp indexer
     indexer = _create_indexer(core, core_index_to_indices, optim_paras)
