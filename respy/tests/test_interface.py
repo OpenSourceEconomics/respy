701b9ef26ec8a200100037e5e2be5013b2bec750
timmens <tim@mensinger96.com>
2020-06-07 12:28:36 +0200

    Add new test case.

--- respy/tests/test_interface.py
+++ respy/tests/test_interface.py
@@ -13,5 +13,11 @@ def test_get_example_model(model):
 
 @pytest.mark.unit
 @pytest.mark.parametrize("model", EXAMPLE_MODELS)
+def test_get_example_model_with_sd(model):
+    params, options = get_example_model(model, with_data=False, with_sd=True)
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize("model", EXAMPLE_MODELS)
 def test_get_parameter_constraints(model):
     get_parameter_constraints(model)
