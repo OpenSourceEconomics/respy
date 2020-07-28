b1d9b2d42bbfba73e0032d3ea57bf960a498fa5b
MaxBlesch <s6mables@uni-bonn.de>
2020-07-28 17:31:06 +0200

    Changed to numpy docstring template.

--- respy/pre_processing/transition_matrix_exog_process.py
+++ respy/pre_processing/transition_matrix_exog_process.py
@@ -14,9 +14,21 @@ def parse_transition_matrix_for_exogenous_processes(matrix, process_name):
 
     Parse a transition matrix to logit coefficients and create params file and
     covariates template for the respy initial files.
-    :param matrix:
-    :param process_name:
-    :return:
+    Parameters
+    ----------
+    matrix : pandas.DataFrame
+        The transition matrix for an exogenous process.
+    process_name : string
+        The name of the exogenous process.
+    Returns
+    -------
+    params : pandas.DataFrame
+        A subset of the params initial file for respy. Contains all information on
+        the exogenous process.
+    covariates : dictionary
+        A template for the specification of the covariates required for the exogenous
+        process.
+
     """
     # Check transition matrix conditions
     check_numerics(matrix)
@@ -33,11 +45,23 @@ def create_params(transformed_matrix, states, process_name, process_states):
     """Create the params entries for the respy initial files.
 
     Create entries with logit coefficents instead of probabilities.
-    :param transformed_matrix:
-    :param states:
-    :param process_name:
-    :param process_states:
-    :return:
+    Parameters
+    ----------
+    transformed_matrix : pandas.DataFrame
+        The original DataFrame converted to logit values.
+    states : list
+        A list of the states on which probabilities for the exogenous process are
+        definded.
+    process_name : string
+        The name of the exogenous process.
+    process_states : list
+        A list of the possible outcomes of the exogenous process.
+
+    Returns
+    -------
+    params : pandas.DataFrame
+        A subset of the params initial file for respy. Contains all information on
+        the exogenous process.
     """
     categories = [
         f"exogenous_process_{process_name}_{state}" for state in process_states
@@ -56,8 +80,15 @@ def create_params(transformed_matrix, states, process_name, process_states):
 def transform_matrix(matrix):
     """Transform probabilities to logit coefficients.
 
-    :param matrix:
-    :return:
+    Parameters
+    ----------
+    matrix : pandas.DataFrame
+        The transition matrix for an exogenous process.
+    Returns
+    -------
+    transformed_matrix : pandas.DataFrame
+        The original DataFrame converted to logit values.
+
     """
     transformed_matrix = matrix.copy()
     transformed_matrix[transformed_matrix == 0] = -1e300
@@ -71,10 +102,22 @@ def transform_matrix(matrix):
 def create_covariates(states, process_name, process_states):
     """Create a covariate template for the user.
 
-    :param states:
-    :param process_name:
-    :param process_states:
-    :return:
+    Parameters
+    ----------
+    states : list
+        A list of the states on which probabilities for the exogenous process are
+        definded.
+    process_name : string
+        The name of the exogenous process.
+    process_states : list
+        A list of the possible outcomes of the exogenous process.
+
+    Returns
+    -------
+    covariates : dictionary
+        A template for the specification of the covariates required for the exogenous
+        process.
+
     """
     covariates = {}
     for state in states:
@@ -97,8 +140,13 @@ def create_covariates(states, process_name, process_states):
 def check_numerics(matrix):
     """Check numeric conditions on a transition matrix.
 
-    :param matrix:
-    :return:
+    Parameters
+    ----------
+    matrix : pandas.DataFrame
+        The transition matrix for an exogenous process.
+    Returns
+    -------
+    Nothing. Only raises errors if conditions are not fullfilled.
     """
     n_states = matrix.shape[0]
     if not np.allclose(matrix.sum(axis=1), np.full(n_states, 1)):
