b9af4f75a133e033e486d613112d236631f2af89
Tobias Raabe <raabe@posteo.de>
2020-08-19 09:21:00 +0200

    Bump version.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ copyright = f"2015-{dt.datetime.now().year}, The respy Development Team"  # noqa
 author = "The respy Development Team"
 
 # The full version, including alpha/beta/rc tags.
-release = "2.0.0dev5"
+release = "2.0.0"
 version = ".".join(release.split(".")[:2])
 
 # -- General configuration ------------------------------------------------
