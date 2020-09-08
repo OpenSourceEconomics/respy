ca8f01f8c0e6272e11f2c77069a795b6ab843322
mo2561057 <moritz.mendel12@gmail.com>
2020-09-08 23:28:02 +0200

    checking

--- respy/tests/test_exogenous_processes.py
+++ respy/tests/test_exogenous_processes.py
@@ -1,13 +1,11 @@
 import numpy as np
 import pandas as pd
-import pytest
 
 from respy.interface import get_example_model
 from respy.simulate import get_simulate_func
 from respy.solve import get_solve_func
 
 
-@pytest.fixture
 def model_with_one_exog_proc():
     params, options = get_example_model("robinson_crusoe_basic", with_data=False)
     params.loc[("nonpec_fishing", "sick"), "value"] = -2
@@ -20,7 +18,6 @@ def model_with_one_exog_proc():
     return params, options
 
 
-@pytest.fixture
 def model_with_two_exog_proc(model_with_one_exog_proc):
     params, options = model_with_one_exog_proc
     params.loc[("nonpec_fishing", "stormy"), "value"] = -1
@@ -66,8 +63,8 @@ def test_weight_continuation_values_for_one_exogenous_process(model_with_one_exo
     solve = get_solve_func(params, options)
     state_space = solve(params)
 
-    state_space.get_attribute("expected_value_functions")[(0,)][:] = 1
-    state_space.get_attribute("expected_value_functions")[(1,)][:] = 2
+    state_space.expected_value_functions[(0,)][:] = 1
+    state_space.expected_value_functions[(1,)][:] = 2
 
     # The weighted continuation value should be 0.9 * 1 + 0.1 * 2 = 1.1.
     for period in range(options["n_periods"] - 1):
@@ -82,10 +79,10 @@ def test_weight_continuation_values_for_two_exog_processes(model_with_two_exog_p
     solve = get_solve_func(params, options)
     state_space = solve(params)
 
-    state_space.get_attribute("expected_value_functions")[(0, 0)][:] = 1
-    state_space.get_attribute("expected_value_functions")[(0, 1)][:] = 2
-    state_space.get_attribute("expected_value_functions")[(1, 0)][:] = 3
-    state_space.get_attribute("expected_value_functions")[(1, 1)][:] = 4
+    state_space.expected_value_functions[(0, 0)][:] = 1
+    state_space.expected_value_functions[(0, 1)][:] = 2
+    state_space.expected_value_functions[(1, 0)][:] = 3
+    state_space.expected_value_functions[(1, 1)][:] = 4
 
     # The weighted continuation value should be
     # 0.9 * 0.8 * 1 + 0.9 * 0.2 * 2 + 0.1 * 0.8 * 3 + 0.1 * 0.2 * 4 = 1.4.
